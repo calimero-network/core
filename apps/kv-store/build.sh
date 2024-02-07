25a58be21b0986cf8e14b2a054398b0420549e5f
Miraculous Owonubi <omiraculous@gmail.com>
2024-02-07 10:38:14 +0100

    add app profile

--- apps/kv-store/build.sh
+++ apps/kv-store/build.sh
@@ -5,6 +5,10 @@ cd "$(dirname $0)"
 
 TARGET="${CARGO_TARGET_DIR:-../../target}"
 
-cargo build --target wasm32-unknown-unknown --release
+cargo build --target wasm32-unknown-unknown --profile app-release
 
 cp $TARGET/wasm32-unknown-unknown/release/kv_store.wasm ./res/
+
+if command -v wasm-opt > /dev/null; then
+  wasm-opt -Oz ./res/kv_store.wasm -o ./res/kv_store.wasm
+fi
