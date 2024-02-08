88ed914d042a66010b6a362ed489e33c7aec32c6
Miraculous Owonubi <omiraculous@gmail.com>
2024-02-08 05:42:27 +0100

    fix build extraction path

--- apps/kv-store/build.sh
+++ apps/kv-store/build.sh
@@ -7,7 +7,7 @@ TARGET="${CARGO_TARGET_DIR:-../../target}"
 
 cargo build --target wasm32-unknown-unknown --profile app-release
 
-cp $TARGET/wasm32-unknown-unknown/release/kv_store.wasm ./res/
+cp $TARGET/wasm32-unknown-unknown/app-release/kv_store.wasm ./res/
 
 if command -v wasm-opt > /dev/null; then
   wasm-opt -Oz ./res/kv_store.wasm -o ./res/kv_store.wasm
