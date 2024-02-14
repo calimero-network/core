458042f6631dd639aed60d8b02d8ca8bb585631a
matej.vukosav <matej.vukosav@gmail.com>
2024-02-14 16:43:31 +0100

    feat: vp, vc

---
+++ identity-rs/src/vault.rs
@@ -0,0 +1,13 @@
+
+
+
+// unlock vault using peer keypair
+
+// encrypt vault using peer keypair
+
+//encrypt vault using some other keypair (wallet)
+//store encryptor public keys for DH
+
+// login with wallet
+
+//create VC
