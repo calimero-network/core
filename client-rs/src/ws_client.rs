2110115e2cc96c23ba57636f8f32d2a120dc001d
Fran Domovic <fran@calimero.network>
2024-02-15 11:52:11 +0100

    feat: refactor - extract logic to functins

--- client-rs/src/ws_client.rs
+++ client-rs/src/ws_client.rs
@@ -1,8 +1,11 @@
-use futures_util::{SinkExt, StreamExt};
 use rand::{Rng, thread_rng};
+
 use serde::{Deserialize, Serialize};
-use tokio_tungstenite::{connect_async, tungstenite::Message};
 use color_eyre::owo_colors::OwoColorize;
+
+use tokio::net::TcpStream;
+use tokio_tungstenite::{connect_async, tungstenite::Message, MaybeTlsStream, WebSocketStream};
+use futures_util::{stream::{SplitSink, SplitStream}, SinkExt, StreamExt};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct JsonRequestSendParams {
@@ -13,52 +16,86 @@ struct JsonRequestSendParams {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct JsonRequestSend {
+struct JsonRequestSendMethod {
     jsonrpc: String,
     id: String,
     method: String,
 }
 
-pub async fn ws_params(ws_address: &String, method: &String, params: Vec<String>) {
+pub struct WSClientStream {
+    pub write: SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>,
+    pub read: SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>
+}
 
-    println!("Connecting to {}", ws_address.green());
+impl WSClientStream {
+    pub async fn get_stream(ws_address: &str) -> Result<Self, Box<dyn std::error::Error>> {
+        println!("Connecting to {}", ws_address.green());
+        let (ws_stream, _) = connect_async(ws_address).await?;
+        let (write,read) = ws_stream.split();
+        Ok(Self {
+            write,
+            read
+         })
+    }
+}
 
-    let (ws_stream, _) = connect_async(ws_address).await.expect("Failed to connect");
-
-    let (mut write, mut read) = ws_stream.split();
-
+fn generate_request_method(method: &String) -> JsonRequestSendMethod {
     let random_int_string_10_chars: String = thread_rng()
     .sample_iter(&rand::distributions::Uniform::new_inclusive(b'0', b'9'))
     .take(10)
     .map(|c| c as char)
     .collect();
 
-    
+    let request_object = JsonRequestSendMethod {
+        jsonrpc: "2.0".to_string(),
+        id: random_int_string_10_chars.clone(),
+        method: method.to_string(),
+    };
+
+    request_object
+}
+
+fn generate_request_params(method: &String, params: Vec<String>) -> JsonRequestSendParams {
+    let random_int_string_10_chars: String = thread_rng()
+    .sample_iter(&rand::distributions::Uniform::new_inclusive(b'0', b'9'))
+    .take(10)
+    .map(|c| c as char)
+    .collect();
+
     let request_object = JsonRequestSendParams {
         jsonrpc: "2.0".to_string(),
         id: random_int_string_10_chars.clone(),
         method: method.to_string(),
         params
     };
+
+    request_object
+}
+
+pub async fn ws_params(ws_address: &String, method: &String, params: Vec<String>) {
+    let mut ws_client_stream = WSClientStream::get_stream(ws_address)
+        .await
+        .expect("Failed to get WebSocket stream");
+
+    let request_object = generate_request_params(method, params);
     
     let json_string_reqest = serde_json::to_string(&request_object).expect("Failed to serialize JSON");
     
-
     let msg = Message::Text(r#json_string_reqest.to_string().into());
 
-    write.send(msg).await.expect("Failed to send message");
+    ws_client_stream.write.send(msg).await.expect("Failed to send message");
 
     loop {
-        if let Some(message) = read.next().await {
+        if let Some(message) = ws_client_stream.read.next().await {
             if let Ok(text) = message.expect("Failed to read message").into_text() {
                 if let Ok(json_request) = serde_json::from_str::<JsonRequestSendParams>(text.as_str()) {
-                    if json_request.id == random_int_string_10_chars {
-                        println!("Received response with id: {}", json_request.id);
-                        println!("Message received: {}", json_request.params.join(" ").green());
+                    if json_request.id == request_object.id {
+                        println!("Received response with id: {} \nMessage received: {}",
+                        json_request.id, json_request.params.join(" ").green());
                         break;
                     }
                 } else {
-                    //
+                    continue
                 }
             }
         }
@@ -66,44 +103,29 @@ pub async fn ws_params(ws_address: &String, method: &String, params: Vec<String>
 }
 
 pub async fn ws_no_params(ws_address: &String, method: &String) {
+    let mut ws_client_stream = WSClientStream::get_stream(ws_address)
+        .await
+        .expect("Failed to get WebSocket stream");
 
-    println!("Connecting to {}", ws_address.green());
+    let request_object = generate_request_method(method);
 
-    let (ws_stream, _) = connect_async(ws_address).await.expect("Failed to connect");
-
-    let (mut write, mut read) = ws_stream.split();
-
-    let random_int_string_10_chars: String = thread_rng()
-    .sample_iter(&rand::distributions::Uniform::new_inclusive(b'0', b'9'))
-    .take(10)
-    .map(|c| c as char)
-    .collect();
-
-    
-    let request_object = JsonRequestSend {
-        jsonrpc: "2.0".to_string(),
-        id: random_int_string_10_chars.clone(),
-        method: method.to_string(),
-    };
-    
     let json_string_reqest = serde_json::to_string(&request_object).expect("Failed to serialize JSON");
-    
 
     let msg = Message::Text(r#json_string_reqest.to_string().into());
 
-    write.send(msg).await.expect("Failed to send message");
+    ws_client_stream.write.send(msg).await.expect("Failed to send message");
 
     loop {
-        if let Some(message) = read.next().await {
+        if let Some(message) = ws_client_stream.read.next().await {
             if let Ok(text) = message.expect("Failed to read message").into_text() {
-                if let Ok(json_request) = serde_json::from_str::<JsonRequestSend>(text.as_str()) {
-                    if json_request.id == random_int_string_10_chars {
+                if let Ok(json_request) = serde_json::from_str::<JsonRequestSendMethod>(text.as_str()) {
+                    if json_request.id == request_object.id {
                         println!("Received response with id: {}", json_request.id);
                         println!("Message received: {}", json_request.method.green());
                         break;
                     }
                 } else {
-                    //
+                   continue;
                 }
             }
         }
