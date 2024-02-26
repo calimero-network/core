d0cb9f3126e8f1022ecd3ab30790a323625f98f7
Filip Bozic <70634661+fbozic@users.noreply.github.com>
2024-02-26 16:39:13 +0100

    fix: remove unused controller primitives

--- crates/primitives/src/controller.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-use crate::api;
-
-#[derive(Serialize, Deserialize, Debug)]
-pub enum Command {
-    WsApiRequest(api::WsClientId, Option<api::WsRequestId>, api::ApiRequest),
-}
--- crates/primitives/src/lib.rs
+++ crates/primitives/src/lib.rs
@@ -1,3 +1,2 @@
 pub mod api;
 pub mod app;
-pub mod controller;
