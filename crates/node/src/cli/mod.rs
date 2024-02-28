ff9cdd5ccf7dbfa3f74861cc5b481b023d14f696
Miraculous Owonubi <omiraculous@gmail.com>
2024-02-28 23:02:36 +0300

    introduce run subcommand

--- crates/node/src/cli/mod.rs
+++ crates/node/src/cli/mod.rs
@@ -1,7 +1,8 @@
-use calimero_node::config::{self, ConfigFile};
-use clap::{Parser, Subcommand, ValueEnum};
+use calimero_node::config;
+use clap::{Parser, Subcommand};
 
 mod init;
+mod run;
 
 #[derive(Debug, Parser)]
 #[clap(author, about, version)]
@@ -10,12 +11,13 @@ pub struct RootCommand {
     pub args: RootArgs,
 
     #[clap(subcommand)]
-    pub action: Option<SubCommands>,
+    pub action: SubCommands,
 }
 
 #[derive(Debug, Subcommand)]
 pub enum SubCommands {
     Init(init::InitCommand),
+    Run(run::RunCommand),
 }
 
 #[derive(Debug, Parser)]
@@ -24,67 +26,13 @@ pub struct RootArgs {
     #[clap(long, value_name = "PATH", default_value_t = config::default_chat_dir())]
     #[clap(env = "CALIMERO_CHAT_HOME", hide_env_values = true)]
     pub home: camino::Utf8PathBuf,
-
-    #[clap(long, value_name = "TYPE")]
-    #[clap(value_enum, default_value_t)]
-    pub node_type: NodeType,
-
-    #[clap(value_name = "PATH")]
-    /// Path to the application (*.wasm file)
-    pub app_path: camino::Utf8PathBuf, // todo! think about this well
-}
-
-#[derive(Copy, Clone, Debug, Default, ValueEnum)]
-pub enum NodeType {
-    #[default]
-    Peer,
-    Coordinator,
-}
-
-impl From<NodeType> for calimero_primitives::types::NodeType {
-    fn from(value: NodeType) -> Self {
-        match value {
-            NodeType::Peer => calimero_primitives::types::NodeType::Peer,
-            NodeType::Coordinator => calimero_primitives::types::NodeType::Coordinator,
-        }
-    }
 }
 
 impl RootCommand {
     pub async fn run(self) -> eyre::Result<()> {
         match self.action {
-            Some(sub_command) => {
-                match sub_command {
-                    SubCommands::Init(init) => init.run(self.args)?,
-                };
-                return Ok(());
-            }
-            None => {}
+            SubCommands::Init(init) => return init.run(self.args),
+            SubCommands::Run(run) => return run.run(self.args).await,
         }
-
-        if !ConfigFile::exists(&self.args.home) {
-            eyre::bail!("chat node is not initialized in {:?}", self.args.home);
-        }
-
-        let config = ConfigFile::load(&self.args.home)?;
-
-        calimero_node::start(calimero_node::NodeConfig {
-            home: self.args.home.clone(),
-            app_path: self.args.app_path,
-            node_type: self.args.node_type.into(),
-            identity: config.identity.clone(),
-            store: calimero_store::config::StoreConfig {
-                path: self.args.home.join(config.store.path),
-            },
-            network: calimero_network::config::NetworkConfig {
-                identity: config.identity,
-                node_type: self.args.node_type.into(),
-                swarm: config.network.swarm,
-                bootstrap: config.network.bootstrap,
-                discovery: config.network.discovery,
-                endpoint: config.network.endpoint,
-            },
-        })
-        .await
     }
 }
