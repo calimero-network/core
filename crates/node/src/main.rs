f64af1391956da1ec127cb1db701f58cd2d4a12f
Filip Bozic <70634661+fbozic@users.noreply.github.com>
2024-03-01 12:26:58 +0100

    chore: revert log changes

--- crates/node/src/main.rs
+++ crates/node/src/main.rs
@@ -16,7 +16,7 @@ async fn main() -> eyre::Result<()> {
 pub fn setup() -> eyre::Result<()> {
     tracing_subscriber::registry()
         .with(EnvFilter::builder().parse(format!(
-            "info,calimero_network=debug,{}",
+            "info,{}",
             // "debug,libp2p_core=warn,libp2p_gossipsub=warn,{}",
             std::env::var("RUST_LOG").unwrap_or_default()
         ))?)
