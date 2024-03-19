0f162e622d8d55e1b88c0ebc8906fbf4cb4c0327
Matej Vukosav <MatejVukosav@users.noreply.github.com>
2024-03-19 19:02:08 +0100

    fix: config borrow (#45)

--- crates/server/src/lib.rs
+++ crates/server/src/lib.rs
@@ -76,15 +76,6 @@ pub async fn start(
         }
     }
 
-    app = app
-        .layer(middleware::auth::AuthSignatureLayer::new(config.identity))
-        .layer(
-            cors::CorsLayer::new()
-                .allow_origin(cors::Any)
-                .allow_headers(cors::Any)
-                .allow_methods([http::Method::POST]),
-        );
-
     #[cfg(feature = "websocket")]
     {
         if let Some((path, handler)) = websocket::service(&config, node_events.clone())? {
@@ -99,6 +90,15 @@ pub async fn start(
 
         return Ok(());
     }
+
+    app = app
+        .layer(middleware::auth::AuthSignatureLayer::new(config.identity))
+        .layer(
+            cors::CorsLayer::new()
+                .allow_origin(cors::Any)
+                .allow_headers(cors::Any)
+                .allow_methods([http::Method::POST]),
+        );
 
     let mut set = tokio::task::JoinSet::new();
 
