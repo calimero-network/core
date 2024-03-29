801c2a228e5297c4c6b8b781b172e5d2570c2e2b
Fran Domovic <fran@calimero.network>
2024-03-29 14:19:13 +0100

    feat: getter for releases

--- package-manager/contract/src/lib.rs
+++ package-manager/contract/src/lib.rs
@@ -2,6 +2,7 @@ use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::store::UnorderedMap;
 use near_sdk::{env, near_bindgen, AccountId, BorshStorageKey};
+use semver;
 
 #[derive(BorshStorageKey, BorshSerialize)]
 #[borsh(crate = "near_sdk::borsh")]
@@ -74,13 +75,36 @@ impl PackageManager {
         path: String,
         hash: String,
     ) {
-        let Some(package) = self.packages.get(&name) else {
-            env::panic_str("Package doesn't exist.");
-        };
-        if package.owner != env::signer_account_id() {
-            env::panic_str("Sender is not the owner of the package");
+        // Get the last release version for the package
+        let last_release_version = self.releases.get(&name).map(|version_map| {
+            version_map
+                .keys()
+                .max_by(|a, b| {
+                    semver::Version::parse(a)
+                        .unwrap()
+                        .cmp(&semver::Version::parse(b).unwrap())
+                })
+                .expect("No versions found for the package")
+        });
+
+        // Check if the last release version exists and is less than the current version
+        if let Some(last_version) = last_release_version {
+            let last_version = semver::Version::parse(&last_version)
+                .expect("Failed to parse last release version");
+            let current_version =
+                semver::Version::parse(&version).expect("Failed to parse current version");
+            if current_version <= last_version {
+                panic!("New release version must be greater than the last release version.");
+            }
         }
 
+        // Check if the sender is the owner of the package
+        let package = self.packages.get(&name).expect("Package doesn't exist.");
+        if package.owner != env::signer_account_id() {
+            panic!("Sender is not the owner of the package");
+        }
+
+        // Insert the new release
         self.releases
             .entry(name.clone())
             .or_insert_with(|| {
@@ -108,8 +132,14 @@ impl PackageManager {
             .collect()
     }
 
-    // TODO: implement `pub fn get_releases(&self) -> Vec<Release> {}`
-    // with pagination (offset+limit) to avoid hitting the gas limit from excessive storage reads
+    pub fn get_releases(&self, offset: usize, limit: usize) -> Vec<Release> {
+        self.releases
+            .values()
+            .flat_map(|version_map| version_map.values().cloned())
+            .skip(offset)
+            .take(limit)
+            .collect()
+    }
 
     pub fn get_package(&self, name: String) -> &Package {
         self.packages.get(&name).expect("Package doesn't exist")
@@ -231,5 +261,52 @@ mod tests {
         assert_eq!(packages_offset1_limit2.len(), 2);
         assert_eq!(packages_offset1_limit2[0].name, "package1".to_string());
         assert_eq!(packages_offset1_limit2[1].name, "package2".to_string());
+    }
+
+    #[test]
+    fn test_get_realses() {
+        let mut contract = PackageManager::default();
+        contract.add_package(
+            "application".to_string(),
+            "Demo Application".to_string(),
+            "https://github.com/application".to_string(),
+        );
+
+        contract.add_package(
+            "package1".to_string(),
+            "Package 1".to_string(),
+            "https://github.com/package1".to_string(),
+        );
+        contract.add_release(
+            "application".to_string(),
+            "0.0.1".to_string(),
+            "".to_string(),
+            "https://gateway/ipfs/CID".to_string(),
+            "123456789".to_string(),
+        );
+        contract.add_release(
+            "application".to_string(),
+            "0.0.2".to_string(),
+            "".to_string(),
+            "https://gateway/ipfs/CID".to_string(),
+            "123456789".to_string(),
+        );
+        contract.add_release(
+            "application".to_string(),
+            "0.1.0".to_string(),
+            "".to_string(),
+            "https://gateway/ipfs/CID".to_string(),
+            "123456789".to_string(),
+        );
+        contract.add_release(
+            "package1".to_string(),
+            "0.1.1".to_string(),
+            "".to_string(),
+            "https://gateway/ipfs/CID".to_string(),
+            "123456789".to_string(),
+        );
+        let relases_versions = contract.get_releases(1, 2);
+        assert_eq!(relases_versions.len(), 2);
+        assert_eq!(relases_versions[0].version, "0.0.2".to_string());
     }
 }
