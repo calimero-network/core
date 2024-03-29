37a2e895833393c35f51359ef29fc2c2de914c79
Fran Domovic <93442516+frdomovic@users.noreply.github.com>
2024-03-29 17:36:12 +0100

    feat(package-manager): add package getter (#55)
    * feat: add package getter
    
    * feat: getter for releases

--- package-manager/contract/src/lib.rs
+++ package-manager/contract/src/lib.rs
@@ -1,9 +1,11 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::serde::{Deserialize, Serialize};
-use near_sdk::store::LookupMap;
+use near_sdk::store::UnorderedMap;
 use near_sdk::{env, near_bindgen, AccountId, BorshStorageKey};
+use semver;
 
 #[derive(BorshStorageKey, BorshSerialize)]
+#[borsh(crate = "near_sdk::borsh")]
 pub enum StorageKeys {
     Packages,
     Release { package: String },
@@ -13,15 +15,16 @@ pub enum StorageKeys {
 // TODO: enable ABI generation support
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
+#[borsh(crate = "near_sdk::borsh")]
 pub struct PackageManager {
-    packages: LookupMap<String, Package>,
-    releases: LookupMap<String, LookupMap<String, Release>>,
+    packages: UnorderedMap<String, Package>,
+    releases: UnorderedMap<String, UnorderedMap<String, Release>>,
 }
 
 //  TODO: add multiple owners
-#[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone)]
 #[serde(crate = "near_sdk::serde")]
+#[borsh(crate = "near_sdk::borsh")]
 pub struct Package {
     name: String,
     description: String,
@@ -32,9 +35,9 @@ pub struct Package {
 // TODO: add a checksum in the future
 // TODO: figure out status of reproduciable builds
 // TODO: add better error checking for URL path
-#[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone)]
 #[serde(crate = "near_sdk::serde")]
+#[borsh(crate = "near_sdk::borsh")]
 pub struct Release {
     version: String,
     notes: String,
@@ -45,8 +48,8 @@ pub struct Release {
 impl Default for PackageManager {
     fn default() -> Self {
         Self {
-            packages: LookupMap::new(StorageKeys::Packages),
-            releases: LookupMap::new(StorageKeys::Releases),
+            packages: UnorderedMap::new(StorageKeys::Packages),
+            releases: UnorderedMap::new(StorageKeys::Releases),
         }
     }
 }
@@ -72,17 +75,40 @@ impl PackageManager {
         path: String,
         hash: String,
     ) {
-        let Some(package) = self.packages.get(&name) else {
-            env::panic_str("Package doesn't exist.");
-        };
-        if package.owner != env::signer_account_id() {
-            env::panic_str("Sender is not the owner of the package");
+        // Get the last release version for the package
+        let last_release_version = self.releases.get(&name).map(|version_map| {
+            version_map
+                .keys()
+                .max_by(|a, b| {
+                    semver::Version::parse(a)
+                        .unwrap()
+                        .cmp(&semver::Version::parse(b).unwrap())
+                })
+                .expect("No versions found for the package")
+        });
+
+        // Check if the last release version exists and is less than the current version
+        if let Some(last_version) = last_release_version {
+            let last_version = semver::Version::parse(&last_version)
+                .expect("Failed to parse last release version");
+            let current_version =
+                semver::Version::parse(&version).expect("Failed to parse current version");
+            if current_version <= last_version {
+                panic!("New release version must be greater than the last release version.");
+            }
         }
 
+        // Check if the sender is the owner of the package
+        let package = self.packages.get(&name).expect("Package doesn't exist.");
+        if package.owner != env::signer_account_id() {
+            panic!("Sender is not the owner of the package");
+        }
+
+        // Insert the new release
         self.releases
             .entry(name.clone())
             .or_insert_with(|| {
-                LookupMap::new(StorageKeys::Release {
+                UnorderedMap::new(StorageKeys::Release {
                     package: name.clone(),
                 })
             })
@@ -97,11 +123,23 @@ impl PackageManager {
             );
     }
 
-    // TODO: implement `pub fn get_packages(&self) -> Vec<Package> {}`
-    // with pagination (offset+limit) to avoid hitting the gas limit from excessive storage reads
+    pub fn get_packages(&self, offset: usize, limit: usize) -> Vec<Package> {
+        self.packages
+            .keys()
+            .skip(offset)
+            .take(limit)
+            .filter_map(|key| self.packages.get(key).cloned())
+            .collect()
+    }
 
-    // TODO: implement `pub fn get_releases(&self) -> Vec<Release> {}`
-    // with pagination (offset+limit) to avoid hitting the gas limit from excessive storage reads
+    pub fn get_releases(&self, offset: usize, limit: usize) -> Vec<Release> {
+        self.releases
+            .values()
+            .flat_map(|version_map| version_map.values().cloned())
+            .skip(offset)
+            .take(limit)
+            .collect()
+    }
 
     pub fn get_package(&self, name: String) -> &Package {
         self.packages.get(&name).expect("Package doesn't exist")
@@ -127,12 +165,12 @@ impl Package {
     }
 }
 
+#[cfg(not(target_arch = "wasm32"))]
 #[cfg(test)]
 mod tests {
-    use near_sdk::test_utils::VMContextBuilder;
-    use near_sdk::{testing_env, MockedBlockchain, VMContext};
-
     use super::*;
+    use near_sdk::test_utils::VMContextBuilder;
+    use near_sdk::{testing_env, VMContext};
 
     fn get_context(is_view: bool) -> VMContext {
         VMContextBuilder::new()
@@ -154,7 +192,7 @@ mod tests {
         );
         let package = contract.get_package("application".to_string());
 
-        assert_eq!(package.owner, "bobo".parse().unwrap());
+        assert_eq!(package.owner, "bobo".to_string());
         assert_eq!(package.name, "application".to_string());
     }
 
@@ -176,5 +214,99 @@ mod tests {
             "https://gateway/ipfs/CID".to_string(),
             "123456789".to_string(),
         );
+    }
+
+    #[test]
+    fn test_get_packages_with_multiple_offsets_and_limits() {
+        let mut contract = PackageManager::default();
+
+        contract.add_package(
+            "application".to_string(),
+            "Demo Application".to_string(),
+            "https://github.com/application".to_string(),
+        );
+
+        contract.add_package(
+            "package1".to_string(),
+            "Package 1".to_string(),
+            "https://github.com/package1".to_string(),
+        );
+
+        contract.add_package(
+            "package2".to_string(),
+            "Package 2".to_string(),
+            "https://github.com/package2".to_string(),
+        );
+
+        // Test with offset 0 and limit 1
+        let packages_offset0_limit1 = contract.get_packages(0, 1);
+        assert_eq!(packages_offset0_limit1.len(), 1);
+        assert_eq!(packages_offset0_limit1[0].owner, "bob.near".to_string());
+        assert_eq!(packages_offset0_limit1[0].name, "application".to_string());
+
+        // Test with offset 1 and limit 1
+        let packages_offset1_limit1 = contract.get_packages(1, 1);
+        assert_eq!(packages_offset1_limit1.len(), 1);
+        assert_eq!(packages_offset1_limit1[0].owner, "bob.near".to_string());
+        assert_eq!(packages_offset1_limit1[0].name, "package1".to_string());
+
+        // Test with offset 0 and limit 2
+        let packages_offset0_limit2 = contract.get_packages(0, 2);
+        assert_eq!(packages_offset0_limit2.len(), 2);
+        assert_eq!(packages_offset0_limit2[0].name, "application".to_string());
+        assert_eq!(packages_offset0_limit2[1].name, "package1".to_string());
+
+        // Test with offset 1 and limit 2
+        let packages_offset1_limit2 = contract.get_packages(1, 2);
+        assert_eq!(packages_offset1_limit2.len(), 2);
+        assert_eq!(packages_offset1_limit2[0].name, "package1".to_string());
+        assert_eq!(packages_offset1_limit2[1].name, "package2".to_string());
+    }
+
+    #[test]
+    fn test_get_realses() {
+        let mut contract = PackageManager::default();
+        contract.add_package(
+            "application".to_string(),
+            "Demo Application".to_string(),
+            "https://github.com/application".to_string(),
+        );
+
+        contract.add_package(
+            "package1".to_string(),
+            "Package 1".to_string(),
+            "https://github.com/package1".to_string(),
+        );
+        contract.add_release(
+            "application".to_string(),
+            "0.0.1".to_string(),
+            "".to_string(),
+            "https://gateway/ipfs/CID".to_string(),
+            "123456789".to_string(),
+        );
+        contract.add_release(
+            "application".to_string(),
+            "0.0.2".to_string(),
+            "".to_string(),
+            "https://gateway/ipfs/CID".to_string(),
+            "123456789".to_string(),
+        );
+        contract.add_release(
+            "application".to_string(),
+            "0.1.0".to_string(),
+            "".to_string(),
+            "https://gateway/ipfs/CID".to_string(),
+            "123456789".to_string(),
+        );
+        contract.add_release(
+            "package1".to_string(),
+            "0.1.1".to_string(),
+            "".to_string(),
+            "https://gateway/ipfs/CID".to_string(),
+            "123456789".to_string(),
+        );
+        let relases_versions = contract.get_releases(1, 2);
+        assert_eq!(relases_versions.len(), 2);
+        assert_eq!(relases_versions[0].version, "0.0.2".to_string());
     }
 }
