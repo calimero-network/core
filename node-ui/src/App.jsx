8453ee6be2f85151fe626e7af732587cefcd1531
Fran Domovic <93442516+frdomovic@users.noreply.github.com>
2024-04-02 15:00:37 +0200

    feat: admin-ui navigation (#67)

--- node-ui/src/App.jsx
+++ node-ui/src/App.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Bootstrap from "./pages/Bootstrap";
 import ConfirmWallet from "./pages/ConfirmWallet";
+import Identity from "./pages/Identity";
 
 export default function App() {
   return (
     <>
-    <BrowserRouter basename="/admin">
-      <Routes>
-      <Route path="/" element={<Bootstrap />}/>
-      <Route path="/confirm-wallet" element={<ConfirmWallet />}/>
-          {/* <Route index element={<Home />} />
-          <Route path="blogs" element={<Blogs />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} /> */}
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter basename="/admin">
+        <Routes>
+          <Route path="/" element={<Bootstrap />} />
+          <Route path="/confirm-wallet" element={<ConfirmWallet />} />
+          <Route path="/identity" element={<Identity />} />
+          <Route path="/applications" element={<Identity />} />
+          <Route path="/keys" element={<Identity />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
