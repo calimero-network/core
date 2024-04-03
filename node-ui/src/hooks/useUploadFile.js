6ae3f22b1a43e1387c2a8563a3e47f67055c52e6
Fran Domovic <fran@calimero.network>
2024-04-03 19:24:05 +0200

    feat: upload files to ipfs

---
+++ node-ui/src/hooks/useUploadFile.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-console */
+import { useState, useCallback } from "react";
+import { useHelia } from "./useHelia";
+
+export const useUploadFile = () => {
+  const { helia, fs, error, starting } = useHelia();
+  const [cid, setCid] = useState(null);
+  const [cidString, setCidString] = useState("");
+
+  const commitWasm = useCallback(
+    async (binary) => {
+      if (!error && !starting) {
+        try {
+          const cid = await fs.addBytes(binary, helia.blockstore);
+          setCid(cid);
+          setCidString(cid.toString());
+          console.log("Added file:", cid.toString());
+        } catch (e) {
+          console.error(e);
+        }
+      } else {
+        console.log("please wait for helia to start");
+      }
+    },
+    [error, starting, helia, fs]
+  );
+
+  const fetchWasm = useCallback(async () => {
+    if (!error && !starting && cid && helia && fs) {
+      try {
+        const fileChunks = [];
+        for await (const chunk of fs.cat(cid)) {
+          fileChunks.push(chunk);
+        }
+
+        const fileBlob = new Blob(fileChunks, { type: "application/wasm" });
+        const fileObject = window.URL.createObjectURL(fileBlob);
+        return fileObject;
+      } catch (e) {
+        console.error(e);
+      }
+    } else {
+      console.log("Please wait for helia to start.");
+    }
+  }, [error, starting, cid, helia, fs]);
+
+  return { cidString, commitWasm, fetchWasm };
+};
