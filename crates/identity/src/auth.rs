a76b94694c0a5117ec568ed90eabebc7e710dfc9
matej.vukosav <matej.vukosav@gmail.com>
2024-04-03 15:27:25 +0200

    replace String with str

--- crates/identity/src/auth.rs
+++ crates/identity/src/auth.rs
@@ -5,7 +5,7 @@ pub fn verify_peer_auth(keypair: &Keypair, msg: &[u8], signature: &[u8]) -> bool
     keypair.public().verify(msg, signature)
 }
 
-pub fn verify_eth_signature(account: String, message: String, signature: String) -> bool {
+pub fn verify_eth_signature(account: &str, message: &str, signature: &str) -> bool {
     let signature_bytes = match hex::decode(signature.trim_start_matches("0x")) {
         Ok(bytes) => bytes,
         Err(_) => return false,
@@ -31,7 +31,7 @@ pub fn verify_eth_signature(account: String, message: String, signature: String)
     }
 }
 
-pub fn eth_message(message: String) -> [u8; 32] {
+pub fn eth_message(message: &str) -> [u8; 32] {
     keccak256(
         format!(
             "{}{}{}",
@@ -51,7 +51,7 @@ mod tests {
     fn test_recover() {
         let account = "0x63f9a92d8d61b48a9fff8d58080425a3012d05c8".to_string();
         let message = "0x63f9a92d8d61b48a9fff8d58080425a3012d05c8igwyk4r1o7o";
-        let message = eth_message(message.to_string());
+        let message = eth_message(message);
         let signature ="0x382a3e04daf88f322730f6a2972475fc5646ea8c4a7f3b5e83a90b10ba08a7364cd2f55348f2b6d210fbed7fc485abf19ecb2f3967e410d6349dd7dd1d4487751b".trim_start_matches("0x");
         let signature = hex::decode(signature).unwrap();
         println!("{} {:?} {:?}", account, message, signature);
