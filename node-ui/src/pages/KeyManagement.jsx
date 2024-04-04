bb16ea0b01661b8011eed20f1794a8615cd71f5e
Fran Domovic <fran@calimero.network>
2024-04-04 17:46:19 +0200

    feat: keys page

--- node-ui/src/pages/KeyManagement.jsx
+++ node-ui/src/pages/KeyManagement.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { Navigation } from "../components/Navigation";
 import { FlexLayout } from "../components/layout/FlexLayout";
+import { KeysContent } from "../components/keyManagement/KeysContent";
+import { KeysTable } from "../components/keyManagement/KeysTable";
 
 export default function Keys() {
   return (
     <FlexLayout>
       <Navigation />
+      <KeysContent>
+        <KeysTable
+          nodeKeys={[]}
+          setActive={() => console.log("set active")}
+          revokeKey={() => console.log("revoke key")}
+        />
+      </KeysContent>
     </FlexLayout>
   );
 }
