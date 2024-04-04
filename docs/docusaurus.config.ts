61f6efb60cf0130e970faadd22936109504cbb3b
Xabi Losada <xabilosada@gmail.com>
2024-04-04 03:42:51 +0200

    feat: build sitemap

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -62,7 +62,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Documentation',
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
