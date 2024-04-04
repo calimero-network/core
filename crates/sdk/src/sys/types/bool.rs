40c2d8ce10d90a5faa82a079a68d1116581dafd3
Miraculous Owonubi <omiraculous@gmail.com>
2024-04-04 01:48:57 +0100

    feat(sdk): no wrapped exchange (#83)

--- crates/sdk/src/sys/types/bool.rs
+++ crates/sdk/src/sys/types/bool.rs
@@ -1,13 +1,15 @@
 #[repr(C)]
 #[derive(Eq, Ord, Copy, Hash, Clone, Debug, PartialEq, PartialOrd)]
-pub struct Bool(u8);
+pub struct Bool(u32);
 
-impl Bool {
-    pub const fn as_bool(self) -> Option<bool> {
-        match self {
-            Self(0) => Some(false),
-            Self(1) => Some(true),
-            _ => None,
+impl TryFrom<Bool> for bool {
+    type Error = u32;
+
+    fn try_from(value: Bool) -> Result<Self, Self::Error> {
+        match value {
+            Bool(0) => Ok(false),
+            Bool(1) => Ok(true),
+            Bool(x) => Err(x),
         }
     }
 }
