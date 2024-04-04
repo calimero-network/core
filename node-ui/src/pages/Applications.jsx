6ba025c97ee6821c7ed914c182fa4928544b9e42
Fran Domovic <fran@calimero.network>
2024-04-04 17:50:43 +0200

    fix: cleanup

--- node-ui/src/pages/Applications.jsx
+++ node-ui/src/pages/Applications.jsx
@@ -7,7 +7,6 @@ import { ApplicationsTable } from "../components/applications/ApplicationsTable"
 import { InstallApplication } from "../components/applications/InstallApplication";
 import { useRPC } from "../hooks/useNear";
 import { useAdminClient } from "../hooks/useAdminClient";
-import { useUploadFile } from "../hooks/useUploadFile";
 
 export default function Applications() {
   const { getPackages, getReleases } = useRPC();
@@ -25,29 +24,7 @@ export default function Applications() {
       })();
     }
   }, [packages]);
-  const { cidString, commitWasm, fetchWasm } = useUploadFile();
 
-  const [myhref, setHref] = useState("");
-  const handleFileChange = (event) => {
-    const file = event.target.files[0];
-    if (file && file.name.endsWith(".wasm")) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        const arrayBuffer = e.target.result;
-        const bytes = new Uint8Array(arrayBuffer);
-        commitWasm(bytes);
-      };
-      reader.readAsArrayBuffer(file);
-    } else {
-      console.log("Please select a .wasm file.");
-    }
-  };
-
-  const handleDownload = async () => {
-    let fileObject = await fetchWasm();
-    console.log("🚀 ~ handleDownload ~ fileObject:", fileObject);
-    setHref(fileObject);
-  };
   return (
     <FlexLayout>
       <Navigation />
@@ -68,10 +45,6 @@ export default function Applications() {
         </ApplicationsContent>
       ) : (
         <ApplicationsContent>
-          <input type="file" accept=".wasm" onChange={handleFileChange} />
-          <h4>{cidString}</h4>
-          <button onClick={handleDownload}>download file</button>
-          <a href={myhref}>download</a>
           <ApplicationsTable
             applications={[]}
             install={() => setSwitchInstall(true)}
