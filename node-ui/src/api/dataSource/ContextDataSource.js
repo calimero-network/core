4847f666fe39f619d8a7aa500def15e567ff5f4d
Fran Domovic <fran@calimero.network>
2024-05-02 16:29:04 +0200

    feat: fix init args passed

--- node-ui/src/api/dataSource/ContextDataSource.js
+++ node-ui/src/api/dataSource/ContextDataSource.js
@@ -1,38 +1,39 @@
 export class ContextDataSource {
-    client;
-  
-    constructor(client) {
-      this.client = client;
-    }
-  
-    async getContexts() {
-      try {
-        const response = await this.client.get("/admin-api/context");
-        if (response?.contexts) {
-          return response.contexts;
-        } else {
-          return [];
-        }
-      } catch (error) {
-        console.error("Error fetching contexts:", error);
+  client;
+
+  constructor(client) {
+    this.client = client;
+  }
+
+  async getContexts() {
+    try {
+      const response = await this.client.get("/admin-api/context");
+      if (response?.contexts) {
+        return response.contexts;
+      } else {
         return [];
       }
-    }
-
-    async startContexts(applicationId, initArguments) {
-        const argumentsStringify = JSON.stringify(initArguments);
-
-        try {
-            const response = await this.client.post("/admin-api/context", {applicationId, argumentsStringify});
-            if (response?.data) {
-            return response.data;
-            } else {
-            return [];
-            }
-        } catch (error) {
-            console.error("Error starting contexts:", error);
-            return [];
-        }
+    } catch (error) {
+      console.error("Error fetching contexts:", error);
+      return [];
     }
   }
-  
+
+  async startContexts(applicationId, initFunction, initArguments) {
+    try {
+      const response = await this.client.post("/admin-api/context", {
+        appId: applicationId,
+        ...(initFunction && { initFunction }),
+        ...(initArguments && { initArgs: JSON.stringify(initArguments) }),
+      });
+      if (response?.data) {
+        return response.data;
+      } else {
+        return [];
+      }
+    } catch (error) {
+      console.error("Error starting contexts:", error);
+      return [];
+    }
+  }
+}
