66943102e69d94de68b43807599f0fd90b84a5e0
Saeed Dadkhah <sa.dadkhah@gmail.com>
2024-05-03 13:25:56 +0330

    Update build scripts to do wasm target installation before compilation. (#249)

--- apps/kv-store/build.sh
+++ apps/kv-store/build.sh
@@ -5,6 +5,8 @@ cd "$(dirname $0)"
 
 TARGET="${CARGO_TARGET_DIR:-../../target}"
 
+rustup target add wasm32-unknown-unknown
+
 cargo build --target wasm32-unknown-unknown --profile app-release
 
 mkdir -p res
