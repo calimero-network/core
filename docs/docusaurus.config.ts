d5784a2d1cf51355c07df54eea7d33fcfb8d910b
matej.vukosav <matej.vukosav@gmail.com>
2024-05-09 15:33:03 +0200

    feat: docs hierarchy

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -1,103 +1,74 @@
-import type { Config } from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
-  url: 'https://calimero-network.github.io',
-  baseUrl: '/core/',
-  organizationName: 'calimero-network',
-  projectName: 'core',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  title: "Calimero 2.0",
+  tagline:
+    "Calimero Network is a framework which enables building fully decentralized applications, ensuring everyone's data privacy.",
+  favicon: "img/favicon.ico",
+  url: "https://calimero-network.github.io",
+  baseUrl: "/core/",
+  organizationName: "calimero-network",
+  projectName: "core",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
   headTags: [
     {
-      tagName: 'script',
+      tagName: "script",
       attributes: {
-        'data-collect-dnt': 'true',
-        src: 'https://scripts.simpleanalyticscdn.com/latest.js',
-        async: 'async',
-        defer: 'defer',
+        "data-collect-dnt": "true",
+        src: "https://scripts.simpleanalyticscdn.com/latest.js",
+        async: "async",
+        defer: "defer",
       },
-    }
+    },
   ],
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          routeBasePath: '/', // Serve the docs at the site's root
+          routeBasePath: "/", // Serve the docs at the site's root
+          breadcrumbs: true,
         },
         blog: false,
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
   ],
   themeConfig: {
     colorMode: {
-      disableSwitch: true
+      disableSwitch: true,
+    },
+    docs: {
+      sidebar: {
+        hideable: true,
+      },
     },
     navbar: {
-      style: 'dark',
+      style: "dark",
       logo: {
-        alt: 'Calimero Network',
-        src: 'img/logo.svg',
+        alt: "Calimero Network",
+        src: "img/logo.svg",
       },
       items: [
         {
-          href: 'https://github.com/calimero-network/core',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/calimero-network/core",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow (TBD)',
-              href: 'https://stackoverflow.com/questions/tagged/calimero-network',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discord.gg/bp7uKv9kBv',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/CalimeroNetwork',
-            },
-            {
-              label: 'Telegram',
-              href: 'https://t.me/+_6h-gJlnXO83OGVk',
-            }
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: 'https://www.calimero.network/blogs',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/calimero-network/core',
-            },
-          ],
-        },
-      ],
+      style: "dark",
       copyright: `Copyright © ${new Date().getFullYear()} Calimero Limited LLC.`,
-    }
+    },
   } satisfies Preset.ThemeConfig,
 };
 
