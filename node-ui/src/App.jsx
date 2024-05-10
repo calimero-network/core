85bd4cc44c50f86639d439d54b44f2f11795203c
Fran Domovic <fran@calimero.network>
2024-05-10 14:31:45 +0200

    fix: revert adding export page

--- node-ui/src/App.jsx
+++ node-ui/src/App.jsx
@@ -9,7 +9,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import UploadApp from "./pages/UploadApp";
 import Contexts from "./pages/Contexts";
 import StartContext from "./pages/StartContext";
-import Export from "./pages/Export";
 
 export default function App() {
   return (
@@ -23,7 +22,6 @@ export default function App() {
           <Route path="/upload-app" element={<UploadApp />} />
           <Route path="/contexts" element={<Contexts />} />
           <Route path="/start-context" element={<StartContext />} />
-          <Route path="/export" element={<Export />} />
         </Routes>
       </BrowserRouter>
     </>
