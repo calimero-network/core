b9532d0e1b0369ebbe8331f5ad07547d25f7f995
Fran Domovic <fran@calimero.network>
2024-05-15 16:52:01 +0200

    fix: eof

--- node-ui/src/api/contextApi.ts
+++ node-ui/src/api/contextApi.ts
@@ -1,8 +1,12 @@
 import { Context, NodeContexts } from "./dataSource/ContextDataSource";
 
 export interface ContextApi {
-    getContexts(): Promise<NodeContexts<Context>>;
-    getContext(contextId: string): Promise<Context>;
-    deleteContext(contextId: string): Promise<Boolean>;
-    startContexts(applicationId: string, initFunction: string, initArguments: string): Promise<boolean>;
-  }
+  getContexts(): Promise<NodeContexts<Context>>;
+  getContext(contextId: string): Promise<Context>;
+  deleteContext(contextId: string): Promise<Boolean>;
+  startContexts(
+    applicationId: string,
+    initFunction: string,
+    initArguments: string
+  ): Promise<boolean>;
+}
