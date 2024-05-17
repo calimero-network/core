d87be472054a30be9841707bc1fbdeca135d8030
petarjuki7 <petar@calimero.network>
2024-05-17 17:57:01 +0200

    New funcionality with PublicKeys and Events

--- apps/commit-reveal-rps/src/lib.rs
+++ apps/commit-reveal-rps/src/lib.rs
@@ -1,198 +1,169 @@
-use std::collections::HashMap;
+use std::cmp::Ordering;
 use std::ops::Deref;
-use std::str::FromStr;
 
-use calimero_sdk::borsh::{BorshDeserialize, BorshSerialize};
-use calimero_sdk::serde::Serialize;
-use calimero_sdk::{app, env};
+use calimero_sdk::app;
+use calimero_sdk::borsh::{io, BorshDeserialize, BorshSerialize};
+use calimero_sdk::serde::{Deserialize, Serialize};
+use ed25519_dalek::{Signature, Signer, SigningKey, Verifier, VerifyingKey, PUBLIC_KEY_LENGTH};
 use sha3::{Digest, Sha3_256};
+
+pub(crate) type Commitment = [u8; 32];
+
+#[derive(Default, Debug)]
+pub struct PublicKey(VerifyingKey);
+
+impl BorshSerialize for PublicKey {
+    fn serialize<W: std::io::Write>(&self, writer: &mut W) -> Result<(), std::io::Error> {
+        let bytes = self.0.to_bytes();
+        writer.write_all(&bytes)?;
+        Ok(())
+    }
+}
+
+impl BorshDeserialize for PublicKey {
+    fn deserialize_reader<R: std::io::prelude::Read>(reader: &mut R) -> std::io::Result<Self> {
+        let mut public_key_bytes = [0u8; PUBLIC_KEY_LENGTH];
+        reader.read_exact(&mut public_key_bytes)?;
+
+        let key = VerifyingKey::from_bytes(&public_key_bytes)
+            .map_err(|_| io::Error::new(io::ErrorKind::InvalidData, "Invalid public key"))?;
+
+        Ok(PublicKey(key))
+    }
+}
+
+impl Serialize for PublicKey {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let bytes = self.0.to_bytes();
+        serializer.serialize_bytes(&bytes)
+    }
+}
 
 #[derive(Debug, Serialize)]
 pub enum Error {
-    NotFound(String),
     ConversionError(String),
-    WrongInput(String),
-    InvalidCall(String),
+    VerifyError(String),
+}
+
+#[derive(Debug, Serialize)]
+pub enum SignError {
+    String,
+}
+
+#[derive(Debug, Serialize)]
+pub enum JoinError {
+    GameFull(String),
+}
+
+#[derive(Debug, Serialize)]
+pub enum CommitError {
+    PlayerNotFound(String),
+    InvalidSignature(String),
+    AlreadyCommitted(String),
+}
+
+#[derive(Debug, Serialize)]
+pub enum RevealError {
+    PlayerNotFound(String),
+    InvalidNonce(String),
+    NotCommitted(String),
+    NotRevealed(String),
 }
 
 #[app::event]
 pub enum Event<'a> {
-    PlayerUpdated { id: usize, hash: &'a [u8; 32] },
-    PlayerInserted { id: usize, hash: &'a [u8; 32] },
+    PlayerCommited {
+        id: usize,
+        hash: &'a [u8; 32],
+    },
+    PlayerInserted {
+        id: usize,
+        public_key: &'a PublicKey,
+        name: &'a str,
+    },
+    PlayerRevealed {
+        id: usize,
+        reveal: &'a Choice,
+    },
+    PlayerWon {
+        id: usize,
+    },
+    StateDumped {},
 }
 
 #[app::state(emits = for<'a> Event<'a>)]
 #[derive(Default, Debug, BorshSerialize, BorshDeserialize)]
 #[borsh(crate = "calimero_sdk::borsh")]
 struct Game {
-    players: HashMap<usize, Player>,
-    ids: bool,
-    commit_state: [bool; 2], //state for the two id's (did they commit)
-    choices: [Option<String>; 2],
-    results: [u8; 3],
+    players: [Option<Player>; 2],
 }
 
 #[app::state]
 #[derive(Default, Debug, BorshSerialize, BorshDeserialize)]
 #[borsh(crate = "calimero_sdk::borsh")]
 struct Player {
-    hash: [u8; 32],
-    reveal_correct: bool,
-    id: usize,
+    state: Option<State>,
+    key: PublicKey,
+    name: String,
 }
 
-#[derive(Debug)]
-enum Choices {
+#[derive(Debug, BorshSerialize, BorshDeserialize)]
+#[borsh(crate = "calimero_sdk::borsh")]
+enum State {
+    Commited(Commitment),
+    Revealed(Choice),
+}
+
+#[derive(Debug, PartialEq, Eq, BorshSerialize, BorshDeserialize, Deserialize, Serialize)]
+#[borsh(crate = "calimero_sdk::borsh")]
+pub enum Choice {
     Rock,
     Paper,
     Scissors,
 }
 
-impl FromStr for Choices {
-    type Err = Error;
-
-    fn from_str(input: &str) -> Result<Choices, Self::Err> {
-        match input {
-            "Rock" => Ok(Choices::Rock),
-            "Paper" => Ok(Choices::Paper),
-            "Scissors" => Ok(Choices::Scissors),
-            _ => Err(Error::ConversionError(
-                "Choose one of Rock, Papar or Scissors".to_string(),
-            )),
-        }
-    }
-}
-
-impl AsRef<[u8]> for Choices {
+impl AsRef<[u8]> for Choice {
     fn as_ref(&self) -> &[u8] {
         match self {
-            Choices::Rock => b"Rock",
-            Choices::Paper => b"Paper",
-            Choices::Scissors => b"Scissors",
+            Choice::Rock => b"Rock",
+            Choice::Paper => b"Paper",
+            Choice::Scissors => b"Scissors",
         }
     }
 }
 
-#[app::logic]
+impl PartialOrd for Choice {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Choice {
+    fn cmp(&self, other: &Self) -> Ordering {
+        use Choice::*;
+
+        match (self, other) {
+            (Rock, Scissors) => Ordering::Greater,
+            (Scissors, Paper) => Ordering::Greater,
+            (Paper, Rock) => Ordering::Greater,
+
+            (Scissors, Rock) => Ordering::Less,
+            (Paper, Scissors) => Ordering::Less,
+            (Rock, Paper) => Ordering::Less,
+
+            _ => Ordering::Equal,
+        }
+    }
+}
+
+//#[app::logic]
 impl Game {
-    pub fn create_player_and_commit(
-        &mut self,
-        choice: &str,
-        salt: &str,
-        id: Option<usize>,
-    ) -> Result<(), Error> {
-        if self.players.len() == 2 && id == None {
-            return Err(Error::WrongInput(
-                "You have to provide your correct id".to_string(),
-            ));
-        }
-
-        if id != None && self.commit_state[id.unwrap()] == true {
-            return Err(Error::InvalidCall("You already commited".to_string()));
-        }
-
-        let hash = Game::calculate_hash(&choice, &salt)?;
-
-        env::log(&format!("New hash is: {:?}", hash));
-
-        //Generate an id
-        let id_insert: usize;
-        match id {
-            Some(id) => {
-                app::emit!(Event::PlayerUpdated {
-                    id: id,
-                    hash: &hash
-                });
-                let player = self
-                    .players
-                    .get_mut(&id)
-                    .ok_or(Error::NotFound("Player with id not found".to_string()))?;
-                player.hash = hash;
-                player.reveal_correct = false;
-
-                self.commit_state[id] = true;
-                return Ok(());
-            }
-            None => match self.ids {
-                true => id_insert = 1,
-                false => {
-                    id_insert = 0;
-                    self.ids = true;
-                }
-            },
-        }
-
-        env::log(&format!("Your id is: {}", id_insert));
-
-        app::emit!(Event::PlayerInserted {
-            id: id_insert,
-            hash: &hash
-        });
-
-        self.players.insert(
-            id_insert,
-            Player {
-                hash,
-                reveal_correct: false,
-                id: id_insert,
-            },
-        );
-
-        self.commit_state[id_insert] = true;
-
-        env::log(&format!("{:?}", hash));
-        Ok(())
-    }
-
-    pub fn reveal(&mut self, salt: &str, id: usize) -> Result<(), Error> {
-        if self.commit_state.into_iter().any(|x| x == false) {
-            return Err(Error::InvalidCall(
-                "Can not reveal yet, one of the players did not commit".to_string(),
-            ));
-        }
-
-        let player = self
-            .players
-            .get_mut(&id)
-            .ok_or(Error::NotFound("Player with id not found".to_string()))?;
-
-        player.reveal_correct = Game::compare_hashes(player.hash, salt).map(|x| {
-            self.choices[id] = Some(x);
-            return true;
-        })?;
-
-        if self.players.values().all(|t| t.reveal_correct == true) {
-            let choice_0 = Choices::from_str(self.choices[0].as_ref().unwrap().as_str())?;
-            let choice_1 = Choices::from_str(self.choices[1].as_ref().unwrap().as_str())?;
-
-            match Game::determine_winner(choice_0, choice_1) {
-                Some(id) => {
-                    self.results[id] += 1;
-
-                    self.commit_state = Default::default();
-                    self.choices = Default::default();
-                    env::log(&format!("Player {} won", id));
-                    Ok(())
-                }
-                None => {
-                    self.results[2] += 1;
-
-                    self.commit_state = Default::default();
-                    self.choices = Default::default();
-                    env::log(&format!("The game ended in a draw"));
-                    Ok(())
-                }
-            }
-        } else {
-            return Err(Error::NotFound(
-                "One of the players salt wasn't correct or didn't reveal yet".to_string(),
-            ));
-        }
-    }
-
-    fn calculate_hash(choice: &str, salt: &str) -> Result<[u8; 32], Error> {
+    fn calculate_hash(choice: &Choice, salt: &str) -> Result<Commitment, Error> {
         Sha3_256::new()
-            .chain_update(Choices::from_str(&choice)?)
+            .chain_update(choice)
             .chain_update(salt)
             .finalize()
             .deref()
@@ -200,37 +171,198 @@ impl Game {
             .map_err(|_| Error::ConversionError("Failed to convert ".to_string()))
     }
 
-    fn compare_hashes(hash: [u8; 32], salt: &str) -> Result<String, Error> {
-        let choices = vec!["Rock", "Paper", "Scissors"];
+    fn compare_hashes(hash: Commitment, salt: &str) -> Result<Choice, Error> {
+        let choices = vec![Choice::Rock, Choice::Paper, Choice::Scissors];
 
         for choice in choices {
-            if Game::calculate_hash(choice, &salt).unwrap() == hash {
-                return Ok(choice.to_string());
+            if Game::calculate_hash(&choice, &salt)? == hash {
+                return Ok(choice);
             }
         }
 
         Err(Error::ConversionError(
-            "No choices match the hash and salt".to_string(),
+            "No Choice match the hash and salt".to_string(),
         ))
     }
 
-    fn determine_winner(choice0: Choices, choice1: Choices) -> Option<usize> {
-        match choice0 {
-            Choices::Rock => match choice1 {
-                Choices::Rock => None,
-                Choices::Paper => Some(1),
-                Choices::Scissors => Some(0),
-            },
-            Choices::Paper => match choice1 {
-                Choices::Rock => Some(0),
-                Choices::Paper => None,
-                Choices::Scissors => Some(1),
-            },
-            Choices::Scissors => match choice1 {
-                Choices::Rock => Some(1),
-                Choices::Paper => Some(0),
-                Choices::Scissors => None,
-            },
+    fn create_keypair(random_bytes: &[u8; 32]) -> SigningKey {
+        SigningKey::from_bytes(random_bytes)
+    }
+
+    fn sign(mut secret_key: SigningKey, message: &[u8]) -> Result<Signature, SignError> {
+        Ok(secret_key.sign(message))
+    }
+
+    fn verify(
+        &self,
+        player_idx: usize,
+        message: &[u8],
+        signature: Signature,
+    ) -> Result<bool, Error> {
+        let signing_key = &self.players[player_idx]
+            .as_ref()
+            .ok_or(Error::VerifyError("Player not found".to_string()))?
+            .key;
+        Ok(signing_key.0.verify(message, &signature).is_ok())
+    }
+
+    pub fn join(&mut self, player_name: String, public_key: PublicKey) -> Result<usize, JoinError> {
+        if let Some((index, slot)) = self
+            .players
+            .iter_mut()
+            .enumerate()
+            .find(|(_, player)| player.is_none())
+        //find an empty spot in the game
+        {
+            app::emit!(Event::PlayerInserted {
+                id: index,
+                public_key: &public_key,
+                name: &player_name
+            });
+
+            let new_player = Player {
+                state: None,
+                key: public_key,
+                name: player_name,
+            };
+
+            *slot = Some(new_player);
+
+            return Ok(index);
+        } else {
+            return Err(JoinError::GameFull(
+                "Two players are already in the game".to_string(),
+            ));
         }
+    }
+
+    fn state(&self) -> [Option<(String, &State)>; 2] {
+        let mut states = [None, None];
+
+        for (i, player) in self.players.as_ref().into_iter().enumerate() {
+            match player {
+                Some(p) => {
+                    if let Some(player_state) = &p.state {
+                        states[i] = Some((p.name.clone(), player_state));
+                    }
+                }
+                None => continue,
+            }
+        }
+        states
+    }
+
+    fn prepare(
+        signing_key: &mut SigningKey,
+        choice: Choice,
+        salt: &str,
+    ) -> Result<(Commitment, Signature), Error> {
+        let hash: Commitment = Game::calculate_hash(&choice, salt)?;
+        let signature = SigningKey::sign(signing_key, &hash);
+        Ok((hash, signature))
+    }
+
+    fn commit(
+        &mut self,
+        player_idx: usize,
+        commitment: Commitment,
+        signature: Signature,
+    ) -> Result<(), CommitError> {
+        let player: &mut Player =
+            self.players[player_idx]
+                .as_mut()
+                .ok_or(CommitError::PlayerNotFound(format!(
+                    "Can't find player with idx {}",
+                    player_idx
+                )))?;
+
+        if let Some(_) = player.state {
+            return Err(CommitError::AlreadyCommitted(
+                "Player already committed".to_string(),
+            ));
+        }
+
+        match player.key.0.verify(&commitment, &signature) {
+            Ok(_) => {
+                app::emit!(Event::PlayerCommited {
+                    id: player_idx,
+                    hash: &commitment
+                });
+                player.state = Some(State::Commited(commitment));
+                return Ok(());
+            }
+            Err(_) => Err(CommitError::InvalidSignature(
+                "Couldn't verify the signature on a commitment".to_string(),
+            )),
+        }
+    }
+
+    fn reveal(&mut self, player_idx: usize, nonce: &str) -> Result<(), RevealError> {
+        let player: &mut Player = self
+            .players
+            .get_mut(player_idx)
+            .ok_or_else(|| {
+                RevealError::PlayerNotFound(format!("Player with id {} not found", player_idx))
+            })?
+            .as_mut()
+            .unwrap(); //this is infallible (?)
+
+        if let Some(State::Commited(commitment)) = player.state {
+            let choice = Game::compare_hashes(commitment, nonce).map_err(|_| {
+                RevealError::InvalidNonce("The nonce provided is invalid".to_string())
+            })?;
+            app::emit!(Event::PlayerRevealed {
+                id: player_idx,
+                reveal: &choice
+            });
+            player.state = Some(State::Revealed(choice));
+        } else {
+            return Err(RevealError::NotCommitted(
+                "Player has not committed yet".to_string(),
+            ));
+        }
+
+        let revealed_choices = self
+            .players
+            .iter()
+            .filter_map(|p| match p {
+                Some(Player {
+                    state: Some(State::Revealed(choice)),
+                    ..
+                }) => Some(choice),
+                _ => None,
+            })
+            .collect::<Vec<&Choice>>();
+
+        if revealed_choices.len() == 2 {
+            let choice0 = *revealed_choices.get(0).ok_or(RevealError::NotRevealed(
+                "Player with index 0 hasn't revealed yet".to_string(),
+            ))?;
+            let choice1 = *revealed_choices.get(1).ok_or(RevealError::NotRevealed(
+                "Player with index 1 hasn't revealed yet".to_string(),
+            ))?;
+
+            if choice0 == choice1 {
+                app::emit!(Event::PlayerWon { id: 3 });
+                return Ok(());
+            }
+            if choice0 > choice1 {
+                app::emit!(Event::PlayerWon { id: 0 });
+                return Ok(());
+            } else {
+                app::emit!(Event::PlayerWon { id: 1 });
+                return Ok(());
+            }
+        } else {
+            return Err(RevealError::NotRevealed(
+                "One of the players hasn't revealed yet".to_string(),
+            ));
+        }
+    }
+
+    fn reset(&mut self) {
+        self.players[0] = None;
+        self.players[1] = None;
+        app::emit!(Event::StateDumped {})
     }
 }
