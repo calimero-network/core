3aaf0dfb25ffc73c274425a4980285404ba85cc5
Miraculous Owonubi <omiraculous@gmail.com>
2024-05-22 02:31:46 +0300

    fix compilation errors

--- apps/rock-paper-scissors/src/key_component.rs
+++ apps/rock-paper-scissors/src/key_component.rs
@@ -1,9 +1,12 @@
+use std::io;
+
+use bs58::decode::DecodeTarget;
 use calimero_sdk::borsh::{BorshDeserialize, BorshSerialize};
-use calimero_sdk::serde::{Deserialize, Deserializer, Serialize};
+use calimero_sdk::serde::{de, ser, Deserialize, Serialize};
 
 use crate::keys::KeyBytes;
 
-#[derive(Default, Debug, PartialEq)]
+#[derive(Debug, Default)]
 pub struct KeyComponent<T: KeyBytes> {
     pub(crate) key: T,
 }
@@ -17,67 +20,53 @@ impl<T: KeyBytes> From<T> for KeyComponent<T> {
 impl<T: KeyBytes> Serialize for KeyComponent<T> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
-        S: calimero_sdk::serde::Serializer,
+        S: ser::Serializer,
     {
-        let bytes = self.key.as_key_bytes();
+        let bytes = self.key.to_bytes();
         let encoded = bs58::encode(bytes).into_string();
         serializer.serialize_str(&encoded)
     }
 }
 
-impl<'de, T: KeyBytes> Deserialize<'de> for KeyComponent<T> {
+impl<'de, T: KeyBytes> Deserialize<'de> for KeyComponent<T>
+where
+    T::Bytes: Default + DecodeTarget,
+{
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
-        D: Deserializer<'de>,
+        D: de::Deserializer<'de>,
     {
-        let encoded = <std::string::String as Deserialize>::deserialize(deserializer)?;
-        let bytes_decoded = bs58::decode(&encoded)
-            .into_vec()
-            .map_err(calimero_sdk::serde::de::Error::custom)?;
+        let encoded = <String as de::Deserialize>::deserialize(deserializer)?;
 
-        let key = match bytes_decoded.len() {
-            32 => {
-                let mut array = [0u8; 32];
-                array.copy_from_slice(&bytes_decoded);
-                T::from_key_bytes(array).map_err(calimero_sdk::serde::de::Error::custom(
-                    "Invalid byte length",
-                ))?
-            }
-            64 => {
-                let mut array = [0u8; 64];
-                array.copy_from_slice(&bytes_decoded);
-                T::from_key_bytes(array).map_err(calimero_sdk::serde::de::Error::custom(
-                    "Invalid byte length",
-                ))?
-            }
-            _ => {
-                return Err(calimero_sdk::serde::de::Error::custom(
-                    "Invalid byte length",
-                ))
-            }
-        };
+        let mut bytes = T::Bytes::default();
+
+        bs58::decode(&encoded)
+            .onto(&mut bytes)
+            .map_err(de::Error::custom)?;
+
+        let key = T::from_bytes(bytes).map_err(|_| de::Error::custom("invalid key"))?;
 
         Ok(KeyComponent { key })
     }
 }
 
-impl<T: KeyBytes> BorshSerialize for KeyComponent<T> {
-    fn serialize<W: std::io::Write>(&self, writer: &mut W) -> Result<(), std::io::Error> {
-        let binding = self.key.as_key_bytes();
-        let bytes = binding.as_ref();
-        writer.write_all(&bytes)?;
-        Ok(())
+impl<T: KeyBytes> BorshSerialize for KeyComponent<T>
+where
+    T::Bytes: BorshSerialize,
+{
+    fn serialize<W: io::Write>(&self, writer: &mut W) -> Result<(), io::Error> {
+        self.key.to_bytes().serialize(writer)
     }
 }
 
 impl<T: KeyBytes> BorshDeserialize for KeyComponent<T>
 where
-    <T as KeyBytes>::Bytes: BorshDeserialize,
+    T::Bytes: BorshDeserialize,
 {
-    fn deserialize_reader<R: std::io::prelude::Read>(reader: &mut R) -> std::io::Result<Self> {
-        let key_bytes = <T::Bytes as BorshDeserialize>::deserialize_reader(reader)?;
+    fn deserialize_reader<R: io::Read>(reader: &mut R) -> io::Result<Self> {
+        let key_bytes = T::Bytes::deserialize_reader(reader)?;
 
-        let key = T::from_key_bytes(key_bytes).map_err(|_| std::io::ErrorKind::InvalidData)?;
+        let key = T::from_bytes(key_bytes).map_err(|_| io::ErrorKind::InvalidData)?;
 
         Ok(KeyComponent { key })
     }
