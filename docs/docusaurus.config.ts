95f33c72fb11b4564e61ddcb3793ec8c3cb0b9fa
Miraculous Owonubi <omiraculous@gmail.com>
2024-05-22 01:31:00 +0300

    add syntax highlighting for bash & toml

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -102,6 +102,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+      additionalLanguages: ["bash", "toml"]
     },
   } satisfies Preset.ThemeConfig,
 };
