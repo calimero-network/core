3e7a051cd4e272b84991225b052086b4ecff8142
Fran Domovic <fran@calimero.network>
2024-05-22 15:54:11 +0200

    feat: update admin ui

--- node-ui/src/App.tsx
+++ node-ui/src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Bootstrap from "./pages/Bootstrap";
-import ConfirmWallet from "./pages/ConfirmWallet";
 import Identity from "./pages/Identity";
 import Applications from "./pages/Applications";
 import Contexts from "./pages/Contexts";
@@ -10,9 +9,11 @@ import ContextDetails from "./pages/ContextDetails";
 import Export from "./pages/Export";
 import ApplicationDetails from "./pages/ApplicationDetails";
 import PublishApplication from "./pages/PublishApplication";
+import AddRelease from "./pages/AddRelease";
+import Near from "./pages/Near";
+import Metamask from "./pages/Metamask";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import AddRelease from "./pages/AddRelease";
 
 export default function App() {
   return (
@@ -20,7 +21,8 @@ export default function App() {
       <BrowserRouter basename="/admin">
         <Routes>
           <Route path="/" element={<Bootstrap />} />
-          <Route path="/confirm-wallet" element={<ConfirmWallet />} />
+          <Route path="/near" element={<Near />} />
+          <Route path="/metamask" element={<Metamask />} />
           <Route path="/identity" element={<Identity />} />
           <Route path="/applications" element={<Applications />} />
           <Route path="/applications/:id" element={<ApplicationDetails />} />
---
+++ node-ui/src/pages/Near.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { WalletSelectorContextProvider } from "@calimero-is-near/calimero-p2p-sdk/lib/wallet/NearLogin/WalletSelectorContext";
+import NearRootKey from "@calimero-is-near/calimero-p2p-sdk/lib/wallet/NearLogin/NearRootKey";
+import ContentWrapper from "../components/login/ContentWrapper";
+
+import "@near-wallet-selector/modal-ui/styles.css";
+
+// @ts-expect-error
+const environment = import.meta.env.VITE_NEAR_ENVIRONMENT ?? "testnet";
+
+export default function Near() {
+  const navigate = useNavigate();
+  return (
+    <ContentWrapper>
+      <WalletSelectorContextProvider network={environment}>
+        <NearRootKey
+          appId={"admin-ui"}
+          rpcBaseUrl={window.location.origin}
+          successRedirect={() => navigate("/identity")}
+          navigateBack={() => navigate("/")}
+          cardBackgroundColor={"#1c1c1c"}
+          nearTitleColor={"white"}
+        />
+      </WalletSelectorContextProvider>
+    </ContentWrapper>
+  );
+}
