8a7438c00ea724374484996a39fcd5c96186a830
Fran Domovic <fran@calimero.network>
2024-05-28 15:28:04 +0200

    fix: localize date function

--- node-ui/src/utils/date.ts
+++ node-ui/src/utils/date.ts
@@ -1,26 +1,14 @@
 export function formatDateWithTime(timestamp: string): string {
   const date = new Date(Number(timestamp));
 
-  const day = date.getDate().toString().padStart(2, "0");
-  const monthNames = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  const month = monthNames[date.getMonth()];
-  const year = date.getFullYear();
+  const formatter = new Intl.DateTimeFormat(undefined, {
+    year: 'numeric',
+    month: 'short',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  });
 
-  const hours = date.getHours().toString().padStart(2, "0");
-  const minutes = date.getMinutes().toString().padStart(2, "0");
-
-  return `${day} ${month} ${year} ${hours}:${minutes}`;
+  return formatter.format(date);
 }
