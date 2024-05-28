5898c43fc159008bf30f559a5ee7787306733223
Fran Domovic <fran@calimero.network>
2024-05-28 12:56:31 +0200

    fix: eof

--- node-ui/src/utils/date.ts
+++ node-ui/src/utils/date.ts
@@ -1,13 +1,26 @@
 export function formatDateWithTime(timestamp: string): string {
-    const date = new Date(Number(timestamp));
-  
-    const day = date.getDate().toString().padStart(2, '0');
-    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    const month = monthNames[date.getMonth()];
-    const year = date.getFullYear();
-  
-    const hours = date.getHours().toString().padStart(2, '0');
-    const minutes = date.getMinutes().toString().padStart(2, '0');
-  
-    return `${day} ${month} ${year} ${hours}:${minutes}`;
-  }
+  const date = new Date(Number(timestamp));
+
+  const day = date.getDate().toString().padStart(2, "0");
+  const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const month = monthNames[date.getMonth()];
+  const year = date.getFullYear();
+
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+
+  return `${day} ${month} ${year} ${hours}:${minutes}`;
+}
