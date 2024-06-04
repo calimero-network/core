37714ef06a7c87cadc260b478c857821152e737e
Fran Domovic <fran@calimero.network>
2024-06-04 20:54:44 +0200

    fix: missing function

--- node-ui/src/utils/date.ts
+++ node-ui/src/utils/date.ts
@@ -12,3 +12,15 @@ export function formatDateWithTime(timestamp: string): string {
 
   return formatter.format(date);
 }
+
+export function formatTimestampToDate(timestamp: number): string {
+  const date = new Date(timestamp);
+
+  const formatter = new Intl.DateTimeFormat(undefined, {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit'
+  });
+
+  return formatter.format(date);
+}
