ea9bd60b30c73a371cf4f5eac7ed1b216c450f36
Miraculous Owonubi <omiraculous@gmail.com>
2024-06-24 15:20:37 +0300

    expand ContextId's capacity

--- crates/primitives/src/context.rs
+++ crates/primitives/src/context.rs
@@ -1,15 +1,19 @@
+use std::fmt;
 use std::ops::Deref;
 
 use serde::{Deserialize, Serialize};
 
 use crate::application::ApplicationId;
+use crate::hash::Hash;
 
 #[derive(Copy, Clone, Debug, Serialize, Deserialize)]
-pub struct ContextId([u8; 32]);
+// todo! define macros that construct newtypes
+// todo! wrapping Hash<N> with this interface
+pub struct ContextId(Hash);
 
 impl From<[u8; 32]> for ContextId {
     fn from(id: [u8; 32]) -> Self {
-        Self(id)
+        Self(id.into())
     }
 }
 
@@ -18,6 +22,18 @@ impl Deref for ContextId {
 
     fn deref(&self) -> &Self::Target {
         &self.0
+    }
+}
+
+impl ContextId {
+    pub fn as_str(&self) -> &str {
+        self.0.as_str()
+    }
+}
+
+impl fmt::Display for ContextId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.pad(self.as_str())
     }
 }
 
