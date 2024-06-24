7b509c6fc442849d4dcc1f9c9b9185f9f0f4c038
petarjuki7 <petar@calimero.network>
2024-06-24 12:10:10 +0200

    Added init, config, run

---
+++ crates/meroctl/src/cli.rs
@@ -0,0 +1,45 @@
+use clap::{Parser, Subcommand};
+
+use crate::config as struct_config;
+
+mod config;
+mod init;
+mod run;
+#[derive(Debug, Parser)]
+#[command(author, about, version)]
+pub struct RootCommand {
+    #[command(flatten)]
+    pub args: RootArgs,
+
+    #[command(subcommand)]
+    pub action: SubCommands,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum SubCommands {
+    Init(init::InitCommand),
+    Config(config::ConfigCommand),
+    Run(run::RunCommand),
+}
+
+#[derive(Debug, Parser)]
+pub struct RootArgs {
+    /// Directory for config and data
+    #[arg(long, value_name = "PATH", default_value_t = struct_config::default_chat_dir())]
+    #[arg(env = "CALIMERO_HOME", hide_env_values = true)]
+    pub home: camino::Utf8PathBuf,
+
+    /// Name of node
+    #[arg(short, long, value_name = "NAME")]
+    pub node_name: camino::Utf8PathBuf,
+}
+
+impl RootCommand {
+    pub async fn run(self) -> eyre::Result<()> {
+        match self.action {
+            SubCommands::Init(init) => return init.run(self.args),
+            SubCommands::Config(config) => return config.run(self.args),
+            SubCommands::Run(run) => return run.run(self.args).await,
+        }
+    }
+}
