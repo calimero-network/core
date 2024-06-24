b37c62598778dfae8d704fa83212733775f596a6
Miraculous Owonubi <omiraculous@gmail.com>
2024-06-24 04:45:04 +0300

    fix readonly construction

--- crates/store/src/layer/read_only.rs
+++ crates/store/src/layer/read_only.rs
@@ -3,24 +3,24 @@ use crate::key::{AsKeyParts, FromKeyParts};
 use crate::layer::{Layer, ReadLayer};
 use crate::slice::Slice;
 
-pub struct ReadOnly<L> {
-    inner: L,
+pub struct ReadOnly<'k, L> {
+    inner: &'k L,
 }
 
-impl<'k, L> ReadOnly<L>
+impl<'k, L> ReadOnly<'k, L>
 where
     L: ReadLayer<'k>,
 {
-    pub fn new(layer: L) -> Self {
+    pub fn new(layer: &'k L) -> Self {
         Self { inner: layer }
     }
 }
 
-impl<L: Layer> Layer for ReadOnly<L> {
+impl<'k, L: Layer> Layer for ReadOnly<'k, L> {
     type Base = L;
 }
 
-impl<'k, L> ReadLayer<'k> for ReadOnly<L>
+impl<'k, L> ReadLayer<'k> for ReadOnly<'k, L>
 where
     L: ReadLayer<'k>,
 {
