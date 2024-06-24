f49087ca73ff0f3500cc2337501cb9c02516625e
Fran Domovic <fran@calimero.network>
2024-06-24 14:21:17 +0200

    fix: update install app handler return type

--- crates/server/src/admin/service.rs
+++ crates/server/src/admin/service.rs
@@ -182,6 +182,11 @@ async fn health_check_handler() -> impl IntoResponse {
     .into_response()
 }
 
+#[derive(Debug, Serialize)]
+struct InstallApplicationResponse {
+    data: bool,
+}
+
 async fn install_application_handler(
     Extension(state): Extension<Arc<AdminState>>,
     Json(req): Json<calimero_server_primitives::admin::InstallApplicationRequest>,
@@ -191,7 +196,10 @@ async fn install_application_handler(
         .install_application(req.application, &req.version)
         .await
     {
-        Ok(()) => ApiResponse { payload: () }.into_response(),
+        Ok(()) => ApiResponse {
+            payload: InstallApplicationResponse { data: true },
+        }
+        .into_response(),
         Err(err) => (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()).into_response(),
     }
 }
