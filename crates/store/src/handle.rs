1b6ba013c462500e6f2c3efdc8e066ff74c5b05a
Miraculous Owonubi <omiraculous@gmail.com>
2024-06-26 14:05:25 +0300

    add layer helpers

--- crates/store/src/handle.rs
+++ crates/store/src/handle.rs
@@ -1,4 +1,4 @@
-use crate::layer::Layer;
+use crate::layer::{read_only, temporal, Layer, ReadLayer, WriteLayer};
 use crate::Store;
 
 mod layer;
@@ -15,5 +15,17 @@ impl<L: Layer> StoreHandle<L> {
     // todo! can remove when/if Layer::commit() -> Layer::Base
     pub fn into_inner(self) -> L {
         self.inner
+    }
+}
+
+impl<'k, L: ReadLayer<'k>> StoreHandle<L> {
+    pub fn read_only(&'k self) -> read_only::ReadOnly<'k, Self> {
+        read_only::ReadOnly::new(self)
+    }
+}
+
+impl<'base, 'k, 'v, L: WriteLayer<'k, 'v>> StoreHandle<L> {
+    pub fn temporal(&'base mut self) -> temporal::Temporal<'base, 'k, 'v, Self> {
+        temporal::Temporal::new(self)
     }
 }
