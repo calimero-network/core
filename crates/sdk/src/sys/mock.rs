e3e746eef252312a285d34a5305d5db098949a0f
Saeed Dadkhah <saeed@zilliqa.com>
2024-06-27 13:23:47 +0330

    add macro

--- crates/sdk/src/sys/mock.rs
+++ crates/sdk/src/sys/mock.rs
@@ -1,62 +1,50 @@
 use super::types::*;
 
-#[no_mangle]
-extern "C" fn panic(_loc: Location) -> ! {
-    panic!("Although your build was successful, it doesn't mean you're allowed to call this function in a non WASM target!")
+macro_rules! non_wasm_panic {
+    (fn $func:ident($($arg:ident: $arg_ty:ty),*$(,)?) $(-> $returns:ty)?;) => {
+        #[no_mangle]
+        extern "C" fn $func($($arg: $arg_ty),*) $(-> $returns)? {
+            panic!("host function `{}` is only available when compiled for wasm32", stringify!($func_name));
+        }
+    };
 }
 
-#[no_mangle]
-extern "C" fn panic_utf8(_msg: Buffer, _loc: Location) -> ! {
-    panic!("Although your build was successful, it doesn't mean you're allowed to call this function in a non WASM target!")
-}
-
-#[no_mangle]
-extern "C" fn register_len(_register_id: RegisterId) -> PtrSizedInt {
-    panic!("Although your build was successful, it doesn't mean you're allowed to call this function in a non WASM target!")
-}
-
-#[no_mangle]
-extern "C" fn read_register(_register_id: RegisterId, _buf: BufferMut) -> Bool {
-    panic!("Although your build was successful, it doesn't mean you're allowed to call this function in a non WASM target!")
-}
-
-#[no_mangle]
-extern "C" fn input(_register_id: RegisterId) {
-    panic!("Although your build was successful, it doesn't mean you're allowed to call this function in a non WASM target!")
-}
-
-#[no_mangle]
-extern "C" fn value_return(_value: ValueReturn) {
-    panic!("Although your build was successful, it doesn't mean you're allowed to call this function in a non WASM target!")
-}
-
-#[no_mangle]
-extern "C" fn log_utf8(_msg: Buffer) {
-    panic!("Although your build was successful, it doesn't mean you're allowed to call this function in a non WASM target!")
-}
-
-#[no_mangle]
-extern "C" fn emit(_event: Event) {
-    panic!("Although your build was successful, it doesn't mean you're allowed to call this function in a non WASM target!")
-}
-
-#[no_mangle]
-extern "C" fn storage_read(_key: Buffer, _register_id: RegisterId) -> Bool {
-    panic!("Although your build was successful, it doesn't mean you're allowed to call this function in a non WASM target!")
-}
-
-#[no_mangle]
-extern "C" fn storage_write(_key: Buffer, _value: Buffer, _register_id: RegisterId) -> Bool {
-    panic!("Although your build was successful, it doesn't mean you're allowed to call this function in a non WASM target!")
-}
-
-#[no_mangle]
-extern "C" fn fetch(
-    _url: Buffer,
-    _method: Buffer,
-    _headers: Buffer,
-    _body: Buffer,
-    _register_id: RegisterId,
-) -> Bool {
-    panic!("Although your build was successful, it doesn't mean you're allowed to call this function in a non WASM target!")
-}
+non_wasm_panic!(
+    fn panic(_loc: Location) -> !;
+);
+non_wasm_panic!(
+    fn panic_utf8(_msg: Buffer, _loc: Location) -> !;
+);
+non_wasm_panic!(
+    fn register_len(_register_id: RegisterId) -> PtrSizedInt;
+);
+non_wasm_panic!(
+    fn read_register(_register_id: RegisterId, _buf: BufferMut) -> Bool;
+);
+non_wasm_panic!(
+    fn input(_register_id: RegisterId);
+);
+non_wasm_panic!(
+    fn value_return(_value: ValueReturn);
+);
+non_wasm_panic!(
+    fn log_utf8(_msg: Buffer);
+);
+non_wasm_panic!(
+    fn emit(_event: Event);
+);
+non_wasm_panic!(
+    fn storage_read(_key: Buffer, _register_id: RegisterId) -> Bool;
+);
+non_wasm_panic!(
+    fn storage_write(_key: Buffer, _value: Buffer, _register_id: RegisterId) -> Bool;
+);
+non_wasm_panic!(
+    fn fetch(
+        _url: Buffer,
+        _method: Buffer,
+        _headers: Buffer,
+        _body: Buffer,
+        _register_id: RegisterId,
+    ) -> Bool;
+);
