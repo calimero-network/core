33f39f63cab68141012da7c8cda6d4ad962159e5
petarjuki7 <petar@calimero.network>
2024-06-27 15:24:07 +0200

    Added example in help message

--- crates/meroctl/src/cli.rs
+++ crates/meroctl/src/cli.rs
@@ -1,4 +1,5 @@
 use clap::{Parser, Subcommand};
+use const_format::concatcp;
 
 use crate::defaults;
 
@@ -6,8 +7,29 @@ mod config;
 mod init;
 mod run;
 
+pub const EXAMPLES: &str = r#"
+  # Initialize a new node
+  $ meroctl --home data/ --node-name node1 init
+
+  # Configure an existing node
+  $ meroctl --home data/ --node-name node1 config --server-host 143.34.182.202 --server-port 3000
+
+  # Run a node as a peer
+  $ meroctl --home data/ --node-name node1 run
+
+  # Run a node as a coordinator
+  $ meroctl --home data/ --node-name node1 run --node-type coordinator
+"#;
+
 #[derive(Debug, Parser)]
-#[command(author, about, version)]
+#[command(author, version, about, long_about = None)]
+#[command(after_help = concatcp!(
+    "Environment variables:\n",
+    "  CALIMERO_HOME    Directory for config and data\n\n",
+    "Examples:",
+    EXAMPLES
+))]
+
 pub struct RootCommand {
     #[command(flatten)]
     pub args: RootArgs,
