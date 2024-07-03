05b971a224d37bd30455290cede5fae1184b3e23
Filip Bozic <70634661+fbozic@users.noreply.github.com>
2024-07-03 11:55:20 +0200

    chore(store): rename TransactionHash type alias

--- crates/store/src/types/context.rs
+++ crates/store/src/types/context.rs
@@ -7,14 +7,14 @@ use crate::key;
 use crate::slice::Slice;
 use crate::types::PredefinedEntry;
 
-pub type Hash = [u8; 32];
+pub type TransactionHash = [u8; 32];
 
 #[derive(Eq, Clone, Debug, PartialEq, BorshSerialize, BorshDeserialize)]
 pub struct ContextMeta {
     // todo! make [u8; 32] when application_id<->meta is a separate record
     pub application_id: Box<str>,
 
-    pub last_transaction_hash: Hash,
+    pub last_transaction_hash: TransactionHash,
 }
 
 impl DataType<'_> for ContextMeta {
@@ -80,7 +80,7 @@ pub struct ContextTransaction {
     pub context_id: [u8; 32],
     pub method: Box<str>,
     pub payload: Box<[u8]>,
-    pub prior_hash: Hash,
+    pub prior_hash: TransactionHash,
 }
 
 impl DataType<'_> for ContextTransaction {
