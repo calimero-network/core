0ffc0d56e1eb14431e65a218007e9085646e341f
Miraculous Owonubi <omiraculous@gmail.com>
2024-07-23 04:40:56 +0100

    yeah..

--- crates/store/src/db.rs
+++ crates/store/src/db.rs
@@ -29,7 +29,7 @@ pub trait Database<'a>: Send + Sync {
     fn get(&self, col: Column, key: Slice) -> eyre::Result<Option<Slice>>;
     fn put(&self, col: Column, key: Slice<'a>, value: Slice<'a>) -> eyre::Result<()>;
     fn delete(&self, col: Column, key: Slice) -> eyre::Result<()>;
-    fn iter(&self, col: Column, key: Slice<'a>) -> eyre::Result<Iter>;
+    fn iter(&self, col: Column) -> eyre::Result<Iter>;
 
     fn apply(&self, tx: &Transaction<'a>) -> eyre::Result<()>;
 }
