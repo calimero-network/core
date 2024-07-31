fafa3922f9dd694bb48f8c830605ca59a7fe4fdd
Miraculous Owonubi <omiraculous@gmail.com>
2024-07-31 02:33:23 +0100

    fix temporal's iterator

--- crates/store/src/layer/temporal.rs
+++ crates/store/src/layer/temporal.rs
@@ -1,8 +1,8 @@
-use crate::iter::{DBIter, Iter, Structured};
+use crate::iter::{DBIter, FusedIter, Iter, Structured};
 use crate::key::{AsKeyParts, FromKeyParts};
 use crate::layer::{Layer, ReadLayer, WriteLayer};
 use crate::slice::Slice;
-use crate::tx::{Operation, Transaction};
+use crate::tx::{self, Operation, Transaction};
 
 pub struct Temporal<'base, 'entry, L> {
     inner: &'base mut L,
@@ -28,11 +28,11 @@ where
     type Base = L;
 }
 
-impl<'base, 'entry, L> ReadLayer<'base> for Temporal<'base, 'entry, L>
+impl<'base, 'entry, L> ReadLayer for Temporal<'base, 'entry, L>
 where
-    L: ReadLayer<'base>,
+    L: ReadLayer,
 {
-    fn has<K: AsKeyParts>(&'base self, key: &'base K) -> eyre::Result<bool> {
+    fn has<K: AsKeyParts>(&self, key: &K) -> eyre::Result<bool> {
         match self.shadow.get(key) {
             Some(Operation::Delete) => Ok(false),
             Some(Operation::Put { .. }) => Ok(true),
@@ -40,7 +40,7 @@ where
         }
     }
 
-    fn get<K: AsKeyParts>(&'base self, key: &'base K) -> eyre::Result<Option<Slice<'base>>> {
+    fn get<K: AsKeyParts>(&self, key: &K) -> eyre::Result<Option<Slice>> {
         match self.shadow.get(key) {
             Some(Operation::Delete) => Ok(None),
             Some(Operation::Put { value }) => Ok(Some(value.into())),
@@ -48,12 +48,12 @@ where
         }
     }
 
-    fn iter<K: FromKeyParts>(&'base self) -> eyre::Result<Iter<Structured<K>>> {
-        // todo! track lifetimes
-
+    fn iter<K: FromKeyParts>(&self) -> eyre::Result<Iter<Structured<K>>> {
         Ok(Iter::new(TemporalIterator {
-            inner: self.inner.iter::<K>()?,
+            inner: FusedIter::new(self.inner.iter::<K>()?),
             shadow: &self.shadow,
+            range: None,
+            value: None,
         }))
     }
 }
@@ -88,33 +88,59 @@ where
 }
 
 struct TemporalIterator<'a, 'b, K> {
-    inner: Iter<'a, Structured<K>>,
+    inner: FusedIter<Iter<'a, Structured<K>>>,
     shadow: &'a Transaction<'b>,
+    range: Option<tx::Iter<'a, 'b>>,
+    value: Option<Slice<'a>>,
 }
 
 impl<'a, 'b, K: AsKeyParts + FromKeyParts> DBIter for TemporalIterator<'a, 'b, K> {
-    fn seek(&mut self, key: Slice) -> eyre::Result<()> {
-        DBIter::seek(&mut self.inner, key)
+    fn seek(&mut self, key: Slice) -> eyre::Result<Option<Slice>> {
+        self.inner.seek(key)
     }
 
     fn next(&mut self) -> eyre::Result<Option<Slice>> {
         loop {
+            self.value = None;
+
             // safety: Slice doesn't mutably borrow self
-            let other = unsafe { &mut *(&mut self.inner as *mut Iter<'a, Structured<K>>) };
+            let other =
+                unsafe { &mut *(&mut self.inner as *mut FusedIter<Iter<'a, Structured<K>>>) };
 
-            let Some(key) = other.next()? else {
-                return Ok(None);
-            };
+            if let Some(key) = other.next()? {
+                match self.shadow.raw_get(K::column(), &key) {
+                    Some(Operation::Delete) => continue,
+                    Some(Operation::Put { value }) => self.value = Some(value.into()),
+                    None => {}
+                }
 
-            match self.shadow.raw_get(K::column(), &key) {
-                Some(Operation::Put { value }) => return Ok(Some(value.as_ref().into())),
-                Some(Operation::Delete) => continue,
-                None => return Ok(Some(key)),
+                return Ok(Some(key));
             }
+
+            let shadow_iter = self.range.get_or_insert_with(|| self.shadow.iter());
+
+            if let Some((entry, op)) = shadow_iter.next() {
+                match op {
+                    Operation::Delete => continue,
+                    Operation::Put { value } => self.value = Some(value.into()),
+                }
+
+                return Ok(Some(entry.key().into()));
+            }
+
+            return Ok(None);
         }
     }
 
     fn read(&self) -> eyre::Result<Slice> {
-        self.inner.read()
+        if let Some(value) = &self.value {
+            return Ok(value.into());
+        };
+
+        let Some(value) = self.inner.read()? else {
+            eyre::bail!("missing value for iterator entry");
+        };
+
+        Ok(value)
     }
 }
