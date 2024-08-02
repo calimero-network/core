127a552496f0f55ccdfb9e8ebc28d7e56f03753a
Miraculous Owonubi <omiraculous@gmail.com>
2024-08-02 15:57:20 +0100

    watcher CLI

--- crates/server/src/admin/handlers/applications.rs
+++ crates/server/src/admin/handlers/applications.rs
@@ -1,9 +1,13 @@
 use std::sync::Arc;
 
+use axum::extract::Path;
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::{Extension, Json};
-use calimero_server_primitives::admin::{ApplicationInstallResult, InstallApplicationResponse};
+use calimero_server_primitives::admin::{
+    ApplicationInstallResult, GetApplicationResponse, GetApplicationResult,
+    InstallApplicationResponse,
+};
 
 use crate::admin::service::{AdminState, ApiResponse};
 
@@ -19,6 +23,21 @@ pub async fn install_dev_application_handler(
         Ok(application_id) => ApiResponse {
             payload: InstallApplicationResponse {
                 data: ApplicationInstallResult { application_id },
+            },
+        }
+        .into_response(),
+        Err(err) => (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()).into_response(),
+    }
+}
+
+pub async fn get_application(
+    Extension(state): Extension<Arc<AdminState>>,
+    Path(application_id): Path<calimero_primitives::application::ApplicationId>,
+) -> impl IntoResponse {
+    match state.ctx_manager.get_application(&application_id) {
+        Ok(application) => ApiResponse {
+            payload: GetApplicationResponse {
+                data: GetApplicationResult { application },
             },
         }
         .into_response(),
