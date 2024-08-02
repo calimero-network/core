e58caaeaf75c6b873da2894b4ead2468442784f3
Miraculous Owonubi <omiraculous@gmail.com>
2024-08-02 03:37:51 +0100

    stage

--- crates/store/src/types/blobs.rs
+++ crates/store/src/types/blobs.rs
@@ -7,7 +7,7 @@ use crate::types::PredefinedEntry;
 #[derive(Eq, Clone, Debug, PartialEq, BorshSerialize, BorshDeserialize)]
 pub struct BlobMeta {
     // todo! impl proper entry reference count
-    pub refs: usize,
+    // pub refs: usize,
     pub size: usize,
     pub links: Box<[key::BlobMeta]>,
 }
