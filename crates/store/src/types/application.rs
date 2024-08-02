3701a0b0b2083e68489ac69c7ab9f3a08ba9140d
Miraculous Owonubi <omiraculous@gmail.com>
2024-08-02 03:37:51 +0100

    stage

--- crates/store/src/types/application.rs
+++ crates/store/src/types/application.rs
@@ -7,9 +7,9 @@ use crate::types::PredefinedEntry;
 #[derive(Eq, Clone, Debug, PartialEq, BorshSerialize, BorshDeserialize)]
 pub struct ApplicationMeta {
     // todo! impl proper entry reference count
-    pub refs: usize,
-    pub source: Source,
-    pub size: usize,
+    // pub refs: usize,
+    pub blob: key::BlobMeta,
+    pub source: Option<Source>,
 }
 
 // registry://near/testnet/miraclx.near/6aaf79da2b1e0a2a5573d48dc11fef1ae82c017d3678da105bed69cc60990142/0.1.0
@@ -25,8 +25,10 @@ pub struct Source {
 #[borsh(use_discriminant = true)]
 #[repr(u8)]
 pub enum Scheme {
-    Registry = 0,
-    Other(Box<str>) = 1,
+    File = 0,
+    Http { secure: bool } = 1,
+    Registry = 2,
+    Other(Box<str>) = 3,
 }
 
 impl PredefinedEntry for key::ApplicationMeta {
