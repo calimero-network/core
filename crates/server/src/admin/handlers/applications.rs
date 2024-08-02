176a4a30d8afa23978ef636b1805423c39526752
Miraculous Owonubi <omiraculous@gmail.com>
2024-08-02 05:54:10 +0100

    update server

--- crates/server/src/admin/handlers/applications.rs
+++ crates/server/src/admin/handlers/applications.rs
@@ -3,6 +3,7 @@ use std::sync::Arc;
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::{Extension, Json};
+use calimero_server_primitives::admin::{ApplicationInstallResult, InstallApplicationResponse};
 
 use crate::admin::service::{AdminState, ApiResponse};
 
@@ -12,10 +13,15 @@ pub async fn install_dev_application_handler(
 ) -> impl IntoResponse {
     match state
         .ctx_manager
-        .install_dev_application(req.application_id, &req.version, req.path)
+        .install_application_from_path(req.path, req.version)
         .await
     {
-        Ok(()) => ApiResponse { payload: () }.into_response(),
+        Ok(application_id) => ApiResponse {
+            payload: InstallApplicationResponse {
+                data: ApplicationInstallResult { application_id },
+            },
+        }
+        .into_response(),
         Err(err) => (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()).into_response(),
     }
 }
