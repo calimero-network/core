73c289fe07d17eba5148ddcf95c9f858024492fa
matej.vukosav <matej.vukosav@gmail.com>
2024-08-23 09:38:00 +0200

    refactor: organize apps code

--- crates/server/src/admin/service.rs
+++ crates/server/src/admin/service.rs
@@ -3,15 +3,10 @@ use std::fmt::{self, Display, Formatter};
 use std::str::{self};
 use std::sync::Arc;
 
-use axum::extract::Path;
 use axum::http::{header, HeaderMap, HeaderValue, StatusCode};
 use axum::response::IntoResponse;
 use axum::routing::{delete, get, post};
-use axum::{Extension, Json, Router};
-use calimero_server_primitives::admin::{
-    ApplicationInstallResult, ApplicationListResult, GetApplicationDetailsResponse,
-    InstallApplicationResponse, ListApplicationsResponse,
-};
+use axum::{Extension, Router};
 use calimero_store::Store;
 use libp2p::identity::Keypair;
 use serde::{Deserialize, Serialize};
@@ -63,11 +58,17 @@ pub(crate) fn setup(
             "/root-key",
             post(handlers::root_keys::create_root_key_handler),
         )
-        .route("/install-application", post(install_application_handler))
-        .route("/applications", get(list_applications_handler))
+        .route(
+            "/install-application",
+            post(handlers::applications::install_application_handler),
+        )
+        .route(
+            "/applications",
+            get(handlers::applications::list_applications_handler),
+        )
         .route(
             "/applications/:app_id",
-            get(get_application_details_handler),
+            get(handlers::applications::get_application_details_handler),
         )
         .route("/did", get(handlers::fetch_did::fetch_did_handler))
         .route("/contexts", post(handlers::context::create_context_handler))
@@ -139,7 +140,10 @@ pub(crate) fn setup(
             "/dev/contexts/:context_id/application",
             post(handlers::context::update_application_id),
         )
-        .route("/dev/applications", get(list_applications_handler))
+        .route(
+            "/dev/applications",
+            get(handlers::applications::list_applications_handler),
+        )
         .layer(Extension(shared_state));
 
     let admin_router = Router::new()
@@ -226,82 +230,6 @@ async fn health_check_handler() -> impl IntoResponse {
         },
     }
     .into_response()
-}
-
-async fn install_application_handler(
-    Extension(state): Extension<Arc<AdminState>>,
-    Json(req): Json<calimero_server_primitives::admin::InstallApplicationRequest>,
-) -> impl IntoResponse {
-    match state
-        .ctx_manager
-        .install_application_from_url(req.url, req.version, req.metadata /*, req.hash */)
-        .await
-    {
-        Ok(application_id) => ApiResponse {
-            payload: InstallApplicationResponse {
-                data: ApplicationInstallResult { application_id },
-            },
-        }
-        .into_response(),
-        Err(err) => (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()).into_response(),
-    }
-}
-
-async fn list_applications_handler(
-    Extension(state): Extension<Arc<AdminState>>,
-) -> impl IntoResponse {
-    let applications = state
-        .ctx_manager
-        .list_installed_applications()
-        .map_err(|err| parse_api_error(err).into_response());
-    match applications {
-        Ok(applications) => {
-            ApiResponse {
-                payload: ListApplicationsResponse {
-                    data: ApplicationListResult { apps: applications },
-                },
-            }
-        }
-        .into_response(),
-        Err(err) => err.into_response(),
-    }
-}
-
-async fn get_application_details_handler(
-    Path(app_id): Path<String>,
-    Extension(state): Extension<Arc<AdminState>>,
-) -> impl IntoResponse {
-    let app_id_result = match app_id.parse() {
-        Ok(app_id) => app_id,
-        Err(_) => {
-            return ApiError {
-                status_code: StatusCode::BAD_REQUEST,
-                message: "Invalid app id".into(),
-            }
-            .into_response();
-        }
-    };
-
-    let application = state
-        .ctx_manager
-        .get_application(&app_id_result)
-        .map_err(|err| parse_api_error(err).into_response());
-
-    match application {
-        Ok(application) => match application {
-            Some(application) => ApiResponse {
-                payload: GetApplicationDetailsResponse { data: application },
-            }
-            .into_response(),
-            None => ApiError {
-                status_code: StatusCode::NOT_FOUND,
-                message: "Context not found".into(),
-            }
-            .into_response(),
-        }
-        .into_response(),
-        Err(err) => err.into_response(),
-    }
 }
 
 async fn certificate_handler(Extension(state): Extension<Arc<AdminState>>) -> impl IntoResponse {
