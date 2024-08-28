51f91cebfe4f6671e37e164c181a9722b056b7ec
Fran Domovic <fran@calimero.network>
2024-08-28 12:55:40 +0200

    fix: cleanup

--- node-ui/src/context/AppLoginContext.tsx
+++ node-ui/src/context/AppLoginContext.tsx
@@ -7,38 +7,94 @@ interface AppLoginProviderProps {
   children: React.ReactNode;
 }
 
+interface AuthorizedPopupProps {
+  applicationIdParam: string | null;
+  callbackParam: string | null;
+  storageApplicationId: string | null;
+  storageCallbackUrl: string | null;
+  setApplicationId: (value: string) => void;
+  setCallbackUrl: (value: string) => void;
+  setShowPopup: (value: boolean) => void;
+}
+
+interface UnAuthorizedPopupProps {
+  applicationIdParam: string | null;
+  callbackParam: string | null;
+  setStorageApplicationId: (value: string) => void;
+  setStorageCallbackUrl: (value: string) => void;
+}
+
 const AppLoginProvider = ({ children }: AppLoginProviderProps) => {
   const { showServerDownPopup } = useServerDown();
   const [applicationId, setApplicationId] = useState('');
   const [callbackUrl, setCallbackUrl] = useState('');
   const [showPopup, setShowPopup] = useState(false);
 
+  const getUrlParams = () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    return {
+      applicationId: urlParams.get('application_id'),
+      callbackUrl: decodeURIComponent(urlParams.get('callback_url') ?? ''),
+    };
+  };
+
+  const handleAuthorizedNode = ({
+    applicationIdParam,
+    callbackParam,
+    storageApplicationId,
+    storageCallbackUrl,
+    setApplicationId,
+    setCallbackUrl,
+    setShowPopup
+  }: AuthorizedPopupProps) => {
+    if (applicationIdParam && callbackParam) {
+      setApplicationId(applicationIdParam);
+      setCallbackUrl(callbackParam);
+      setShowPopup(true);
+    } else if (storageApplicationId && storageCallbackUrl) {
+      setApplicationId(storageApplicationId);
+      setCallbackUrl(storageCallbackUrl);
+      setShowPopup(true);
+    }
+  };
+
+  const handleUnauthorizedNode = ({
+    applicationIdParam,
+    callbackParam,
+    setStorageApplicationId,
+    setStorageCallbackUrl
+  }: UnAuthorizedPopupProps) => {
+    if (applicationIdParam && callbackParam) {
+      setStorageApplicationId(applicationIdParam);
+      setStorageCallbackUrl(callbackParam);
+    }
+  };
+
   useEffect(() => {
     const setupLoginPopup = () => {
       try {
-        const urlParams = new URLSearchParams(window.location.search);
-        const applicationIdParam = urlParams.get('application_id');
-        const callbackParam = decodeURIComponent(
-          urlParams.get('callback_url') ?? '',
-        );
+        const { applicationId: applicationIdParam, callbackUrl: callbackParam } = getUrlParams();
         const isNodeAuthorized = getStorageNodeAuthorized();
         const storageApplicationId = getStorageApplicationId();
         const storageCallbackUrl = getStorageCallbackUrl();
+
         if (isNodeAuthorized) {
-          if (applicationIdParam && callbackParam) {
-            setApplicationId(applicationIdParam);
-            setCallbackUrl(callbackParam);
-            setShowPopup(true);
-          } else if (storageApplicationId && storageCallbackUrl) {
-            setApplicationId(storageApplicationId);
-            setCallbackUrl(storageCallbackUrl);
-            setShowPopup(true);
-          }
+          handleAuthorizedNode({
+            applicationIdParam,
+            callbackParam,
+            storageApplicationId,
+            storageCallbackUrl,
+            setApplicationId,
+            setCallbackUrl,
+            setShowPopup
+        });
         } else {
-          if (applicationIdParam && callbackParam) {
-            setStorageApplicationId(applicationIdParam);
-            setStorageCallbackUrl(callbackParam);
-          }
+          handleUnauthorizedNode({
+            applicationIdParam,
+            callbackParam,
+            setStorageApplicationId,
+            setStorageCallbackUrl
+        });
         }
       } catch (e) {
         console.error(e);
