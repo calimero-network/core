da4197107a442bba1c2ac6ca87d1052f1dc3c8ab
Fran Domovic <fran@calimero.network>
2024-09-20 17:23:45 +0200

    feat: cleanup sdk

--- packages/calimero-sdk/src/api/dataSource/NodeApiDataSource.ts
+++ packages/calimero-sdk/src/api/dataSource/NodeApiDataSource.ts
@@ -1,84 +1,17 @@
 import { ApiResponse } from '../../types/api-response';
 import {
-  ContextResponse,
   HealthRequest,
   HealthStatus,
   JwtTokenResponse,
-  LoginRequest,
-  LoginResponse,
   NodeApi,
-  NodeChallenge,
-  RootKeyRequest,
-  RootKeyResponse,
 } from '../nodeApi';
 import { HttpClient } from '../httpClient';
-import { Header, createAuthHeader } from '../../auth/headers';
 
 export class NodeApiDataSource implements NodeApi {
   private client: HttpClient;
 
   constructor(client: HttpClient) {
     this.client = client;
-  }
-
-  async requestChallenge(
-    rpcBaseUrl: string,
-    contextId: string,
-  ): ApiResponse<NodeChallenge> {
-    return await this.client.post<NodeChallenge>(
-      `${rpcBaseUrl}/admin-api/request-challenge`,
-      {
-        contextId,
-      },
-    );
-  }
-
-  async login(
-    loginRequest: LoginRequest,
-    rpcBaseUrl: string,
-  ): ApiResponse<LoginResponse> {
-    console.log('Send request to node with params', loginRequest);
-
-    return await this.client.post<LoginRequest>(
-      `${rpcBaseUrl}/admin-api/add-client-key`,
-      {
-        ...loginRequest,
-      },
-    );
-  }
-
-  async getContextIdentity(
-    rpcBaseUrl: string,
-    contextId: string,
-    networkId: string = 'mainnet',
-  ): ApiResponse<ContextResponse> {
-    const headers: Header | null = await createAuthHeader(contextId, networkId);
-
-    return await this.client.get<ContextResponse>(
-      `${rpcBaseUrl}/admin-api/contexts/${contextId}/identities`,
-      headers,
-    );
-  }
-
-  async addRootKey(
-    rootKeyRequest: RootKeyRequest,
-    rpcBaseUrl: string,
-    contextId: string,
-  ): ApiResponse<RootKeyResponse> {
-    console.log('Send request to node with params', rootKeyRequest);
-
-    const headers: Header | null = await createAuthHeader(
-      JSON.stringify(rootKeyRequest),
-      contextId,
-    );
-
-    return await this.client.post<LoginRequest>(
-      `${rpcBaseUrl}/admin-api/root-key`,
-      {
-        ...rootKeyRequest,
-      },
-      headers,
-    );
   }
 
   async refreshToken(
--- packages/calimero-sdk/src/index.ts
+++ packages/calimero-sdk/src/index.ts
@@ -1,6 +1,5 @@
 export * from './rpc';
 export * from './subscriptions';
-export * from './wallets';
 export * from './setup';
 export * from './components';
 export * from './types';
