b1b769d55d47fdf4e315674705f4cb2dc67ca35e
Fran Domovic <93442516+frdomovic@users.noreply.github.com>
2024-09-25 20:05:39 +0200

    feat(sdk): refactor calimero-sdk (#755)

--- packages/calimero-sdk/src/api/nodeApi.ts
+++ packages/calimero-sdk/src/api/nodeApi.ts
@@ -1,151 +1,5 @@
 import { ApiResponse } from '../types/api-response';
 
-enum AlgorithmType {
-  Ed25519,
-}
-
-interface WalletTypeBase<T extends Uppercase<string>> {
-  type: T;
-}
-
-interface ETHWalletType extends WalletTypeBase<'ETH'> {
-  chainId: number;
-}
-
-interface NEARWalletType extends WalletTypeBase<'NEAR'> {}
-
-interface SNWalletType extends WalletTypeBase<'STARKNET'> {}
-
-export type WalletType = ETHWalletType | NEARWalletType | SNWalletType;
-
-export namespace WalletType {
-  export function NEAR({ networkId }: { networkId: string }): WalletType {
-    return { type: 'NEAR', networkId } as NEARWalletType;
-  }
-
-  export function ETH({ chainId = 1 }: { chainId?: number }): WalletType {
-    return { type: 'ETH', chainId } as ETHWalletType;
-  }
-
-  export function STARKNET({
-    walletName = 'MS',
-  }: {
-    walletName?: string;
-  }): WalletType {
-    return { type: 'STARKNET', walletName } as SNWalletType;
-  }
-}
-
-enum VerifiableCredentialType {
-  Wallet,
-}
-
-interface WalletVerifiableCredential {
-  wallet_type: WalletType;
-  address: String;
-  public_key: number[];
-  peer_id: String;
-}
-interface VerifiableCredential {
-  algorithm_type: AlgorithmType;
-  credential_subject: VerifiableCredentialType | WalletVerifiableCredential;
-  proof: number[];
-}
-// @ts-ignore
-interface VerifiablePresentation {
-  challenge: String;
-  verifiable_credential: VerifiableCredential;
-  signature: number[];
-}
-
-export interface LoginRequest {
-  walletSignature: String;
-  payload: Payload;
-  walletMetadata: WalletMetadata;
-  contextId: String;
-}
-
-export interface RootKeyRequest {
-  walletSignature: String;
-  payload: Payload;
-  walletMetadata: WalletMetadata;
-  contextId?: String;
-}
-
-export interface NodeChallenge {
-  nonce: String;
-  contextId: String;
-  timestamp: number;
-  nodeSignature: String;
-}
-
-export interface NearMetadata extends WalletMetadata {
-  type: NEARWalletType;
-  signingKey: 'e.g.: ed25519:DfRy7qn3upQS4KFTLChpMG9DmiR29zDMdR1YuUG7cYML';
-}
-
-export interface EthMetadata extends WalletMetadata {
-  type: ETHWalletType;
-  signingKey: 'e.g.: 0x63f9a92d8d61b48a9fff8d58080425a3012d05c8';
-}
-
-export interface SignatureMessage {
-  nodeSignature: String;
-  publicKey: String;
-}
-
-export interface SignatureMessageMetadata {
-  publicKey: String;
-  nodeSignature: String;
-  nonce: String;
-  contextId: String;
-  timestamp: number;
-  message: string; //signed message by wallet
-}
-
-export interface Payload {
-  message: SignatureMessageMetadata;
-  metadata: SignatureMetadata;
-}
-
-export interface NetworkMetadata {
-  chainId: String;
-  rpcUrl: String;
-}
-
-export interface WalletMetadata {
-  wallet: WalletType;
-  verifyingKey: String;
-  walletAddress?: String;
-  networkMetadata?: NetworkMetadata;
-}
-
-export interface SignatureMetadata {
-  //
-}
-
-export interface NearSignatureMessageMetadata extends SignatureMetadata {
-  recipient: String;
-  callbackUrl: String;
-  nonce: String;
-}
-
-export interface EthSignatureMessageMetadata extends SignatureMetadata {
-  //
-}
-
-export interface StarknetSignatureMessageMetadata extends SignatureMetadata {
-  //
-}
-
-export interface WalletSignatureData {
-  payload: Payload | undefined;
-  publicKey: String | undefined;
-}
-
-export interface LoginResponse {}
-export interface RootKeyResponse {}
-
 export interface HealthRequest {
   url: string;
 }
@@ -154,34 +8,12 @@ export interface HealthStatus {
   status: string;
 }
 
-export interface ContextResponse {
-  identities: string[];
-}
-
 export interface JwtTokenResponse {
   access_token: string;
   refresh_token: string;
 }
 
 export interface NodeApi {
-  login(
-    loginRequest: LoginRequest,
-    rpcBaseUrl: string,
-  ): ApiResponse<LoginResponse>;
-  requestChallenge(
-    rpcBaseUrl: string,
-    contextId: string,
-  ): ApiResponse<NodeChallenge>;
-  addRootKey(
-    rootKeyRequest: RootKeyRequest,
-    rpcBaseUrl: string,
-    contextId: string,
-  ): ApiResponse<RootKeyResponse>;
-  getContextIdentity(
-    rpcBaseUrl: string,
-    contextId: string,
-    networkId?: string,
-  ): ApiResponse<ContextResponse>;
   refreshToken(
     refreshToken: string,
     rpcBaseUrl: string,
--- packages/calimero-sdk/src/auth/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './headers';
-export * from './ed25519';
