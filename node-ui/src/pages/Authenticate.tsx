19a87b280cfdc850b72c18a81de16a229634278c
Fran Domovic <93442516+frdomovic@users.noreply.github.com>
2024-10-03 19:51:40 +0200

    Update Authenticate.tsx
    Signed-off-by: Fran Domovic <93442516+frdomovic@users.noreply.github.com>

--- node-ui/src/pages/Authenticate.tsx
+++ node-ui/src/pages/Authenticate.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import ContentWrapper from '../components/login/ContentWrapper';
-import LoginSelector from '../components/login/wallets/LoginSelector';
 
 export default function AuthenticatePage() {
   const navigate = useNavigate();
   return (
     <ContentWrapper>
-      <LoginSelector
-        navigateMetamaskLogin={() => navigate('/auth/metamask')}
-        navigateNearLogin={() => navigate('/auth/near')}
-        navigateStarknetLogin={() => navigate('/auth/starknet')}
-        navigateIcpLogin={() => navigate('/auth/icp')}
-      />
+     <div>testing</div>
     </ContentWrapper>
   );
 }
