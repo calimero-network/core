acd70144e1c39b39a7907e6edf356725c4b51e31
Filip Bozic <70634661+fbozic@users.noreply.github.com>
2024-10-09 15:53:54 +0200

    example(runtime): add skeleton for crdt runtime example

---
+++ crates/runtime/examples/crdt.rs
@@ -0,0 +1,68 @@
+#![allow(unused_crate_dependencies, reason = "Not actually unused")]
+
+use std::env;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+
+use calimero_runtime::logic::{VMContext, VMLimits};
+use calimero_runtime::store::InMemoryStorage;
+use calimero_runtime::{run, Constraint};
+use eyre::Result as EyreResult;
+use owo_colors::OwoColorize;
+use serde_json::{json, to_vec as to_json_vec};
+
+fn main() -> EyreResult<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: {args:?} <path-to-wasm>");
+        return Ok(());
+    }
+
+    let path = &args[1];
+    let path = Path::new(path);
+    if !path.exists() {
+        eyre::bail!("KV wasm file not found");
+    }
+
+    let file = File::open(path)?.bytes().collect::<Result<Vec<u8>, _>>()?;
+
+    let mut storage = InMemoryStorage::default();
+
+    let limits = VMLimits::new(
+        /*max_stack_size:*/ 200 << 10, // 200 KiB
+        /*max_memory_pages:*/ 1 << 10, // 1 KiB
+        /*max_registers:*/ 100,
+        /*max_register_size:*/ (100 << 20).validate()?, // 100 MiB
+        /*max_registers_capacity:*/ 1 << 30, // 1 GiB
+        /*max_logs:*/ 100,
+        /*max_log_size:*/ 16 << 10, // 16 KiB
+        /*max_events:*/ 100,
+        /*max_event_kind_size:*/ 100,
+        /*max_event_data_size:*/ 16 << 10, // 16 KiB
+        /*max_storage_key_size:*/ (1 << 20).try_into()?, // 1 MiB
+        /*max_storage_value_size:*/ (10 << 20).try_into()?, // 10 MiB
+    );
+
+    let cx = VMContext::new(
+        to_json_vec(&json!({
+                  "Add": {
+                    "id": "::library",
+                    "type": "Library",
+                    "data": "ola"
+                  }
+                }
+        ))?,
+        [0; 32],
+    );
+    let get_outcome = run(&file, "apply_action", cx, &mut storage, &limits)?;
+    dbg!(get_outcome);
+
+    println!("{}", "--".repeat(20).dimmed());
+    println!("{:>35}", "Now, let's inspect the storage".bold());
+    println!("{}", "--".repeat(20).dimmed());
+
+    dbg!(storage);
+
+    Ok(())
+}
