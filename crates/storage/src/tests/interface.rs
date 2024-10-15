ac40273a997a982ce0482cee85cc49fb12aebbab
Dan Williams <dan@dotfive.co.uk>
2024-10-15 01:25:16 +0100

    chore: Removed use of Interface from Data (#774)
      - Use of Interface has been removed from the Data trait. This is to
        allow a potential further decoupling whereby the Data trait might be
        supplied more independently (under investigation). This is therefore
        a precursor to a potential removal of hashing operations from the
        Data trait.
    
      - Removed Data.calculate_full_merkle_hash() and moved its logic to
        Interface.calculate_merkle_hash_for(), which previously called it.
    
      - Data.calculate_merkle_hash() has been retained for now, and may be
        removed in a future update. Data.calculate_merkle_hash_for_child()
        has been added to complement it.
    
      - Changed Data.collections() to return a BTreeMap due to the changes
        in logic, as order is now important in that logic due to the changes
        made.

--- crates/storage/src/tests/interface.rs
+++ crates/storage/src/tests/interface.rs
@@ -24,80 +24,6 @@ mod interface__public_methods {
     use super::*;
 
     #[test]
-    fn calculate_merkle_hash_for__empty_record() {
-        let timestamp = 1_765_432_100_123_456_789;
-        let (db, _dir) = create_test_store();
-        let interface = Interface::new(db);
-        let mut element = Element::new(&Path::new("::root::node::leaf").unwrap());
-        element.set_id(TEST_ID[0]);
-        element.metadata.set_created_at(timestamp);
-        element.metadata.updated_at = timestamp;
-        let data = EmptyData {
-            storage: element.clone(),
-        };
-
-        let hash = interface.calculate_merkle_hash_for(&data, false).unwrap();
-        assert_eq!(
-            hex::encode(hash),
-            "173f9a17aa3c6acdad8cdaf06cea4aa4eb7c87cb99e07507a417d6588e679607"
-        );
-    }
-
-    #[test]
-    fn calculate_merkle_hash_for__with_children() {
-        let timestamp = 1_765_432_100_123_456_789;
-        let (db, _dir) = create_test_store();
-        let interface = Interface::new(db);
-        let mut element = Element::new(&Path::new("::root::node").unwrap());
-        element.set_id(TEST_ID[0]);
-        element.metadata.set_created_at(1_765_432_100_123_456_789);
-        element.metadata.updated_at = timestamp;
-        let mut page = Page::new_from_element("Node", element);
-        assert!(interface.save(page.id(), &mut page).unwrap());
-        let child1 = Element::new(&Path::new("::root::node::leaf1").unwrap());
-        let child2 = Element::new(&Path::new("::root::node::leaf2").unwrap());
-        let child3 = Element::new(&Path::new("::root::node::leaf3").unwrap());
-        let mut para1 = Paragraph::new_from_element("Leaf1", child1);
-        let mut para2 = Paragraph::new_from_element("Leaf2", child2);
-        let mut para3 = Paragraph::new_from_element("Leaf3", child3);
-        para1.element_mut().set_id(TEST_ID[1]);
-        para2.element_mut().set_id(TEST_ID[2]);
-        para3.element_mut().set_id(TEST_ID[3]);
-        para1.element_mut().metadata.set_created_at(timestamp);
-        para2.element_mut().metadata.set_created_at(timestamp);
-        para3.element_mut().metadata.set_created_at(timestamp);
-        para1.element_mut().metadata.updated_at = timestamp;
-        para2.element_mut().metadata.updated_at = timestamp;
-        para3.element_mut().metadata.updated_at = timestamp;
-        assert!(interface.save(para1.id(), &mut para1).unwrap());
-        assert!(interface.save(para2.id(), &mut para2).unwrap());
-        assert!(interface.save(para3.id(), &mut para3).unwrap());
-        page.paragraphs.child_info = vec![
-            ChildInfo::new(para1.id(), para1.element().merkle_hash()),
-            ChildInfo::new(para2.id(), para2.element().merkle_hash()),
-            ChildInfo::new(para3.id(), para3.element().merkle_hash()),
-        ];
-        assert!(interface.save(page.id(), &mut page).unwrap());
-
-        assert_eq!(
-            hex::encode(interface.calculate_merkle_hash_for(&para1, false).unwrap()),
-            "9c4d6363cca5bdb5829f0aa832b573d6befd26227a0e2c3cc602edd9fda88db1",
-        );
-        assert_eq!(
-            hex::encode(interface.calculate_merkle_hash_for(&para2, false).unwrap()),
-            "449f30903c94a488f1767b91bc6626fafd82189130cf41e427f96df19a727d8b",
-        );
-        assert_eq!(
-            hex::encode(interface.calculate_merkle_hash_for(&para3, false).unwrap()),
-            "43098decf78bf10dc4c31191a5f59d277ae524859583e48689482c9ba85c5f61",
-        );
-        assert_eq!(
-            hex::encode(interface.calculate_merkle_hash_for(&page, false).unwrap()),
-            "7593806c462bfadd97ed5228a3a60e492cce4b725f2c0e72e6e5b0f7996ee394",
-        );
-    }
-
-    #[test]
     fn children_of() {
         let (db, _dir) = create_test_store();
         let interface = Interface::new(db);
@@ -166,7 +92,7 @@ mod interface__public_methods {
     }
 
     #[test]
-    fn test_save__basic() {
+    fn save__basic() {
         let (db, _dir) = create_test_store();
         let interface = Interface::new(db);
         let element = Element::new(&Path::new("::root::node::leaf").unwrap());
@@ -176,7 +102,7 @@ mod interface__public_methods {
     }
 
     #[test]
-    fn test_save__multiple() {
+    fn save__multiple() {
         let (db, _dir) = create_test_store();
         let interface = Interface::new(db);
         let element1 = Element::new(&Path::new("::root::node1").unwrap());
@@ -191,7 +117,7 @@ mod interface__public_methods {
     }
 
     #[test]
-    fn test_save__not_dirty() {
+    fn save__not_dirty() {
         let (db, _dir) = create_test_store();
         let interface = Interface::new(db);
         let element = Element::new(&Path::new("::root::node::leaf").unwrap());
@@ -204,7 +130,7 @@ mod interface__public_methods {
     }
 
     #[test]
-    fn test_save__too_old() {
+    fn save__too_old() {
         let (db, _dir) = create_test_store();
         let interface = Interface::new(db);
         let element1 = Element::new(&Path::new("::root::node::leaf").unwrap());
@@ -213,14 +139,15 @@ mod interface__public_methods {
         let id = para1.id();
 
         assert!(interface.save(id, &mut para1).unwrap());
-        para1.element_mut().update();
         para2.element_mut().update();
+        sleep(Duration::from_millis(1));
+        para1.element_mut().update();
         assert!(interface.save(id, &mut para1).unwrap());
         assert!(!interface.save(id, &mut para2).unwrap());
     }
 
     #[test]
-    fn test_save__update_existing() {
+    fn save__update_existing() {
         let (db, _dir) = create_test_store();
         let interface = Interface::new(db);
         let element = Element::new(&Path::new("::root::node::leaf").unwrap());
@@ -236,21 +163,21 @@ mod interface__public_methods {
 
     #[test]
     #[ignore]
-    fn test_save__update_merkle_hash() {
+    fn save__update_merkle_hash() {
         // TODO: This is best done when there's data
         todo!()
     }
 
     #[test]
     #[ignore]
-    fn test_save__update_merkle_hash_with_children() {
+    fn save__update_merkle_hash_with_children() {
         // TODO: This is best done when there's data
         todo!()
     }
 
     #[test]
     #[ignore]
-    fn test_save__update_merkle_hash_with_parents() {
+    fn save__update_merkle_hash_with_parents() {
         // TODO: This is best done when there's data
         todo!()
     }
@@ -373,8 +300,8 @@ mod interface__comparison {
         let mut foreign = local.clone();
 
         assert!(interface.save(local.id(), &mut local).unwrap());
-        foreign.element_mut().merkle_hash = foreign
-            .calculate_full_merkle_hash(&interface, false)
+        foreign.element_mut().merkle_hash = interface
+            .calculate_merkle_hash_for(&foreign, false)
             .unwrap();
         assert_eq!(
             local.element().merkle_hash(),
@@ -398,8 +325,8 @@ mod interface__comparison {
         local.element_mut().update();
 
         assert!(interface.save(local.id(), &mut local).unwrap());
-        foreign.element_mut().merkle_hash = foreign
-            .calculate_full_merkle_hash(&interface, false)
+        foreign.element_mut().merkle_hash = interface
+            .calculate_merkle_hash_for(&foreign, false)
             .unwrap();
 
         let result = interface.compare_trees(&foreign).unwrap();
@@ -421,8 +348,8 @@ mod interface__comparison {
         let mut foreign = Page::new_from_element("Test Page", element);
 
         assert!(interface.save(local.id(), &mut local).unwrap());
-        foreign.element_mut().merkle_hash = foreign
-            .calculate_full_merkle_hash(&interface, false)
+        foreign.element_mut().merkle_hash = interface
+            .calculate_merkle_hash_for(&foreign, false)
             .unwrap();
 
         // Make foreign newer
@@ -483,18 +410,17 @@ mod interface__comparison {
         assert!(interface.save(local_page.id(), &mut local_page).unwrap());
         assert!(interface.save(local_para1.id(), &mut local_para1).unwrap());
         assert!(interface.save(local_para2.id(), &mut local_para2).unwrap());
-        foreign_page.element_mut().merkle_hash = foreign_page
-            .calculate_full_merkle_hash(&interface, false)
+        foreign_page.element_mut().merkle_hash = interface
+            .calculate_merkle_hash_for(&foreign_page, false)
             .unwrap();
-        foreign_para1.element_mut().merkle_hash = foreign_para1
-            .calculate_full_merkle_hash(&interface, false)
+        foreign_para1.element_mut().merkle_hash = interface
+            .calculate_merkle_hash_for(&foreign_para1, false)
             .unwrap();
-        foreign_para3.element_mut().merkle_hash = foreign_para3
-            .calculate_full_merkle_hash(&interface, false)
+        foreign_para3.element_mut().merkle_hash = interface
+            .calculate_merkle_hash_for(&foreign_para3, false)
             .unwrap();
 
-        let (local_actions, mut foreign_actions) = interface.compare_trees(&foreign_page).unwrap();
-        foreign_actions.sort();
+        let (local_actions, foreign_actions) = interface.compare_trees(&foreign_page).unwrap();
 
         assert_eq!(
             local_actions,
@@ -509,12 +435,12 @@ mod interface__comparison {
         assert_eq!(
             foreign_actions,
             vec![
+                // Para1 needs comparison due to different hash
+                Action::Compare(local_para1.id()),
                 // Para2 needs to be added to foreign
                 Action::Add(local_para2.id(), to_vec(&local_para2).unwrap()),
                 // Para3 needs to be added locally, but we don't have the data, so we compare
                 Action::Compare(foreign_para3.id()),
-                // Para1 needs comparison due to different hash
-                Action::Compare(local_para1.id()),
             ]
         );
 
@@ -543,5 +469,177 @@ mod interface__comparison {
             )]
         );
         assert_eq!(foreign_para3_actions, vec![]);
+    }
+}
+
+#[cfg(test)]
+mod hashing {
+    use super::*;
+
+    #[test]
+    fn calculate_merkle_hash_for__empty_record() {
+        let timestamp = 1_765_432_100_123_456_789;
+        let (db, _dir) = create_test_store();
+        let interface = Interface::new(db);
+        let mut element = Element::new(&Path::new("::root::node::leaf").unwrap());
+        element.set_id(TEST_ID[0]);
+        element.metadata.set_created_at(timestamp);
+        element.metadata.updated_at = timestamp;
+        let data = EmptyData {
+            storage: element.clone(),
+        };
+
+        let hash = interface.calculate_merkle_hash_for(&data, false).unwrap();
+        assert_eq!(
+            hex::encode(hash),
+            "173f9a17aa3c6acdad8cdaf06cea4aa4eb7c87cb99e07507a417d6588e679607"
+        );
+    }
+
+    #[test]
+    fn calculate_merkle_hash_for__with_children() {
+        let timestamp = 1_765_432_100_123_456_789;
+        let (db, _dir) = create_test_store();
+        let interface = Interface::new(db);
+        let mut element = Element::new(&Path::new("::root::node").unwrap());
+        element.set_id(TEST_ID[0]);
+        element.metadata.set_created_at(1_765_432_100_123_456_789);
+        element.metadata.updated_at = timestamp;
+        let mut page = Page::new_from_element("Node", element);
+        assert!(interface.save(page.id(), &mut page).unwrap());
+        let child1 = Element::new(&Path::new("::root::node::leaf1").unwrap());
+        let child2 = Element::new(&Path::new("::root::node::leaf2").unwrap());
+        let child3 = Element::new(&Path::new("::root::node::leaf3").unwrap());
+        let mut para1 = Paragraph::new_from_element("Leaf1", child1);
+        let mut para2 = Paragraph::new_from_element("Leaf2", child2);
+        let mut para3 = Paragraph::new_from_element("Leaf3", child3);
+        para1.element_mut().set_id(TEST_ID[1]);
+        para2.element_mut().set_id(TEST_ID[2]);
+        para3.element_mut().set_id(TEST_ID[3]);
+        para1.element_mut().metadata.set_created_at(timestamp);
+        para2.element_mut().metadata.set_created_at(timestamp);
+        para3.element_mut().metadata.set_created_at(timestamp);
+        para1.element_mut().metadata.updated_at = timestamp;
+        para2.element_mut().metadata.updated_at = timestamp;
+        para3.element_mut().metadata.updated_at = timestamp;
+        assert!(interface.save(para1.id(), &mut para1).unwrap());
+        assert!(interface.save(para2.id(), &mut para2).unwrap());
+        assert!(interface.save(para3.id(), &mut para3).unwrap());
+        page.paragraphs.child_info = vec![
+            ChildInfo::new(para1.id(), para1.element().merkle_hash()),
+            ChildInfo::new(para2.id(), para2.element().merkle_hash()),
+            ChildInfo::new(para3.id(), para3.element().merkle_hash()),
+        ];
+        assert!(interface.save(page.id(), &mut page).unwrap());
+
+        assert_eq!(
+            hex::encode(interface.calculate_merkle_hash_for(&para1, false).unwrap()),
+            "9c4d6363cca5bdb5829f0aa832b573d6befd26227a0e2c3cc602edd9fda88db1",
+        );
+        assert_eq!(
+            hex::encode(interface.calculate_merkle_hash_for(&para2, false).unwrap()),
+            "449f30903c94a488f1767b91bc6626fafd82189130cf41e427f96df19a727d8b",
+        );
+        assert_eq!(
+            hex::encode(interface.calculate_merkle_hash_for(&para3, false).unwrap()),
+            "43098decf78bf10dc4c31191a5f59d277ae524859583e48689482c9ba85c5f61",
+        );
+        assert_eq!(
+            hex::encode(interface.calculate_merkle_hash_for(&page, false).unwrap()),
+            "7593806c462bfadd97ed5228a3a60e492cce4b725f2c0e72e6e5b0f7996ee394",
+        );
+    }
+
+    #[test]
+    fn calculate_merkle_hash_for__cached_values() {
+        let (db, _dir) = create_test_store();
+        let interface = Interface::new(db);
+        let element = Element::new(&Path::new("::root::node").unwrap());
+        let mut page = Page::new_from_element("Node", element);
+        assert!(interface.save(page.id(), &mut page).unwrap());
+        assert_eq!(interface.children_of(&page.paragraphs).unwrap(), vec![]);
+
+        let child1 = Element::new(&Path::new("::root::node::leaf1").unwrap());
+        let child2 = Element::new(&Path::new("::root::node::leaf2").unwrap());
+        let child3 = Element::new(&Path::new("::root::node::leaf3").unwrap());
+        let mut para1 = Paragraph::new_from_element("Leaf1", child1);
+        let mut para2 = Paragraph::new_from_element("Leaf2", child2);
+        let mut para3 = Paragraph::new_from_element("Leaf3", child3);
+        assert!(interface.save(para1.id(), &mut para1).unwrap());
+        assert!(interface.save(para2.id(), &mut para2).unwrap());
+        assert!(interface.save(para3.id(), &mut para3).unwrap());
+        page.paragraphs.child_info = vec![
+            ChildInfo::new(para1.id(), para1.element().merkle_hash()),
+            ChildInfo::new(para2.id(), para2.element().merkle_hash()),
+            ChildInfo::new(para3.id(), para3.element().merkle_hash()),
+        ];
+        assert!(interface.save(page.id(), &mut page).unwrap());
+
+        let mut hasher0 = Sha256::new();
+        hasher0.update(page.id().as_bytes());
+        hasher0.update(&to_vec(&page.title).unwrap());
+        hasher0.update(&to_vec(&page.element().metadata()).unwrap());
+        let expected_hash0: [u8; 32] = hasher0.finalize().into();
+
+        let mut hasher1 = Sha256::new();
+        hasher1.update(para1.id().as_bytes());
+        hasher1.update(&to_vec(&para1.text).unwrap());
+        hasher1.update(&to_vec(&para1.element().metadata()).unwrap());
+        let expected_hash1: [u8; 32] = hasher1.finalize().into();
+        let mut hasher1b = Sha256::new();
+        hasher1b.update(expected_hash1);
+        let expected_hash1b: [u8; 32] = hasher1b.finalize().into();
+
+        let mut hasher2 = Sha256::new();
+        hasher2.update(para2.id().as_bytes());
+        hasher2.update(&to_vec(&para2.text).unwrap());
+        hasher2.update(&to_vec(&para2.element().metadata()).unwrap());
+        let expected_hash2: [u8; 32] = hasher2.finalize().into();
+        let mut hasher2b = Sha256::new();
+        hasher2b.update(expected_hash2);
+        let expected_hash2b: [u8; 32] = hasher2b.finalize().into();
+
+        let mut hasher3 = Sha256::new();
+        hasher3.update(para3.id().as_bytes());
+        hasher3.update(&to_vec(&para3.text).unwrap());
+        hasher3.update(&to_vec(&para3.element().metadata()).unwrap());
+        let expected_hash3: [u8; 32] = hasher3.finalize().into();
+        let mut hasher3b = Sha256::new();
+        hasher3b.update(expected_hash3);
+        let expected_hash3b: [u8; 32] = hasher3b.finalize().into();
+
+        let mut hasher = Sha256::new();
+        hasher.update(&expected_hash0);
+        hasher.update(&expected_hash1b);
+        hasher.update(&expected_hash2b);
+        hasher.update(&expected_hash3b);
+        let expected_hash: [u8; 32] = hasher.finalize().into();
+
+        assert_eq!(page.calculate_merkle_hash().unwrap(), expected_hash0);
+        assert_eq!(
+            interface.calculate_merkle_hash_for(&para1, false).unwrap(),
+            expected_hash1b
+        );
+        assert_eq!(
+            interface.calculate_merkle_hash_for(&para2, false).unwrap(),
+            expected_hash2b
+        );
+        assert_eq!(
+            interface.calculate_merkle_hash_for(&para3, false).unwrap(),
+            expected_hash3b
+        );
+        assert_eq!(
+            interface.calculate_merkle_hash_for(&page, false).unwrap(),
+            expected_hash
+        );
+    }
+
+    #[test]
+    #[ignore]
+    fn calculate_merkle_hash_for__recalculated_values() {
+        // TODO: Later, tests should be added for recalculating the hashes, and
+        // TODO: especially checking when the data has been interfered with or
+        // TODO: otherwise arrived at an invalid state.
+        todo!()
     }
 }
