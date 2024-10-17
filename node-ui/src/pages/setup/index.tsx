98b63ce7421f571742adb767addb9685a7eeefff
Fran Domovic <fran@calimero.network>
2024-10-17 13:13:00 +0200

    fix(node-ui): clearning local storage in login process

--- node-ui/src/pages/setup/index.tsx
+++ node-ui/src/pages/setup/index.tsx
@@ -4,17 +4,20 @@ import { setAppEndpointKey } from '../../utils/storage';
 import ContentWrapper from '../../components/login/ContentWrapper';
 import { SetupModal } from '../../components/setup/SetupModal';
 import { getNodeUrl } from '../../utils/node';
+import ErrorWrapper from '../../components/setup/ErrorWrapper';
 
 export default function SetupPage() {
   const navigate = useNavigate();
 
   return (
     <ContentWrapper>
-      <SetupModal
-        successRoute={() => navigate('/auth')}
-        setNodeUrl={setAppEndpointKey}
-        getNodeUrl={getNodeUrl}
-      />
+      <ErrorWrapper>
+        <SetupModal
+          successRoute={() => navigate('/auth')}
+          setNodeUrl={setAppEndpointKey}
+          getNodeUrl={getNodeUrl}
+        />
+      </ErrorWrapper>
     </ContentWrapper>
   );
 }
