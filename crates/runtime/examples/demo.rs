3d7257d5a5150c8a4ef4e1a6bb63865842bad2fb
Miraculous Owonubi <omiraculous@gmail.com>
2024-10-19 20:19:12 +0300

    update the kv-store runtime example

--- crates/runtime/examples/demo.rs
+++ crates/runtime/examples/demo.rs
@@ -1,5 +1,6 @@
 #![allow(unused_crate_dependencies, reason = "Not actually unused")]
 
+use core::str;
 use std::env;
 use std::fs::File;
 use std::io::Read;
@@ -10,7 +11,28 @@ use calimero_runtime::store::InMemoryStorage;
 use calimero_runtime::{run, Constraint};
 use eyre::Result as EyreResult;
 use owo_colors::OwoColorize;
-use serde_json::{json, to_vec as to_json_vec};
+use serde_json::{json, to_vec as to_json_vec, Value};
+
+fn parse_payload(payload: &[u8], pretty: bool) -> EyreResult<String> {
+    match serde_json::from_slice::<Value>(payload) {
+        Ok(json) => {
+            return (if pretty {
+                serde_json::to_string_pretty
+            } else {
+                serde_json::to_string
+            })(&json)
+            .map_err(Into::into)
+        }
+        Err(_) => {}
+    };
+
+    match str::from_utf8(&payload) {
+        Ok(string) => return Ok(string.to_owned()),
+        Err(_) => {}
+    };
+
+    Ok(format!("{:?}", payload))
+}
 
 fn main() -> EyreResult<()> {
     let args: Vec<String> = env::args().collect();
@@ -44,62 +66,109 @@ fn main() -> EyreResult<()> {
         /*max_storage_value_size:*/ (10 << 20).try_into()?, // 10 MiB
     );
 
-    let cx = VMContext::new(
-        to_json_vec(&json!({
-            "key": "foo"
-        }))?,
-        [0; 32],
-        [0; 32],
-    );
-    let get_outcome = run(&file, "get", cx, &mut storage, &limits)?;
-    dbg!(get_outcome);
+    let mut execute = |name: &str, payload: Option<Value>| -> EyreResult<()> {
+        println!("{}", "--".repeat(20).dimmed());
+        println!(
+            "method: {}\nparams: {}",
+            name.bold(),
+            payload
+                .as_ref()
+                .map(ToString::to_string)
+                .unwrap_or_else(|| "{}".to_owned())
+                .bold()
+        );
 
-    let cx = VMContext::new(
-        to_json_vec(&json!({
-            "key": "foo",
-            "value": "bar"
-        }))?,
-        [0; 32],
-        [0; 32],
-    );
-    let set_outcome = run(&file, "set", cx, &mut storage, &limits)?;
-    dbg!(set_outcome);
+        let cx = VMContext::new(
+            payload
+                .map(|p| to_json_vec(&p))
+                .transpose()?
+                .unwrap_or_default(),
+            [0; 32],
+            [0; 32],
+        );
 
-    let cx = VMContext::new(
-        to_json_vec(&json!({
-            "key": "foo"
-        }))?,
-        [0; 32],
-        [0; 32],
-    );
-    let get_outcome = run(&file, "get", cx, &mut storage, &limits)?;
-    dbg!(get_outcome);
+        let outcome = run(&file, name, cx, &mut storage, &limits)?;
 
-    let cx = VMContext::new(
-        to_json_vec(&json!({
-            "key": "food"
-        }))?,
-        [0; 32],
-        [0; 32],
-    );
-    let get_result_outcome = run(&file, "get_result", cx, &mut storage, &limits)?;
-    dbg!(get_result_outcome);
+        // dbg!(&outcome);
 
-    let cx = VMContext::new(
-        to_json_vec(&json!({
-            "key": "food"
-        }))?,
-        [0; 32],
-        [0; 32],
-    );
-    let get_unchecked_outcome = run(&file, "get_unchecked", cx, &mut storage, &limits)?;
-    dbg!(get_unchecked_outcome);
+        println!("Logs:");
 
-    println!("{}", "--".repeat(20).dimmed());
-    println!("{:>35}", "Now, let's inspect the storage".bold());
-    println!("{}", "--".repeat(20).dimmed());
+        if outcome.logs.is_empty() {
+            println!("  <no logs>");
+        }
 
-    dbg!(storage);
+        for log in outcome.logs {
+            println!("  | {}", log);
+        }
+
+        println!("Events:");
+
+        if outcome.events.is_empty() {
+            println!("  <no events>");
+        }
+
+        for event in outcome.events {
+            println!("  kind: {}", event.kind);
+            println!("  data: {}", parse_payload(&event.data, false)?);
+        }
+
+        match outcome.returns {
+            Ok(returns) => {
+                println!("Returns:");
+
+                let payload = returns
+                    .as_ref()
+                    .map(|p| parse_payload(p, true))
+                    .transpose()?
+                    .unwrap_or_else(|| "  <no reponse>".to_owned());
+
+                println!("{}", payload);
+            }
+            Err(err) => {
+                println!("Error:");
+                println!(
+                    "{}",
+                    match err {
+                        calimero_runtime::errors::FunctionCallError::ExecutionError(payload) => {
+                            parse_payload(&payload, true)?
+                        }
+                        _ => format!("{:?}", err),
+                    }
+                );
+            }
+        }
+
+        Ok(())
+    };
+
+    execute("init", None)?;
+
+    execute("get", Some(json!({ "key": "foo" })))?;
+
+    execute("set", Some(json!({ "key": "foo", "value": "bar" })))?;
+    execute("get", Some(json!({ "key": "foo" })))?;
+
+    execute("entries", None)?;
+
+    execute("set", Some(json!({ "key": "foo", "value": "baz" })))?;
+    execute("get", Some(json!({ "key": "foo" })))?;
+
+    execute("set", Some(json!({ "key": "name", "value": "Jane" })))?;
+    execute("get", Some(json!({ "key": "name" })))?;
+
+    execute("entries", None)?;
+
+    execute("get_result", Some(json!({ "key": "foo" })))?;
+    execute("get_result", Some(json!({ "key": "height" })))?;
+
+    execute("get_unchecked", Some(json!({ "key": "name" })))?;
+    execute("get_unchecked", Some(json!({ "key": "age" })))?;
+
+    // println!("{}", "--".repeat(20).dimmed());
+    // println!("{:>35}", "Now, let's inspect the storage".bold());
+    // println!("{}", "--".repeat(20).dimmed());
+
+    // dbg!(storage);
 
     Ok(())
 }
