00ea923242425edd70f8e36ad439cbc2a541d76f
Miraculous Owonubi <omiraculous@gmail.com>
2024-10-20 23:18:50 +0300

    detach from Iter lifetime via Slice::into_boxed

--- crates/node/src/interactive_cli/state.rs
+++ crates/node/src/interactive_cli/state.rs
@@ -15,11 +15,15 @@ impl StateCommand {
     pub async fn run(self, node: &Node) -> Result<()> {
         let ind = ">>".blue();
         let handle = node.store.handle();
-        let iter = handle.iter::<ContextStateKey>()?;
+        let mut iter = handle.iter::<ContextStateKey>()?;
 
         println!("{ind} {:44} | {:44}", "State Key", "Value");
 
-        let context_id = ContextId::from(self.context_id);
+        let context_id = match self.context_id.parse::<ContextId>() {
+            Ok(id) => id,
+            Err(e) => eyre::bail!("{} Failed to parse context_id: {}", ind.red(), e),
+        };
+
         let first = 'first: {
             let Some(k) = iter
                 .seek(ContextStateKey::new(context_id, [0; 32].into()))
@@ -28,15 +32,19 @@ impl StateCommand {
                 break 'first None;
             };
 
-            Some((k, iter.read()))
+            Some((k, iter.read().map(|s| s.value.into_boxed())))
         };
 
-        for (k, v) in first.into_iter().chain(iter.entries()) {
+        let rest = iter
+            .entries()
+            .map(|(k, v)| (k, v.map(|s| s.value.into_boxed())));
+
+        for (k, v) in first.into_iter().chain(rest) {
             let (k, v) = (k?, v?);
             if k.context_id() != context_id {
                 break;
             }
-            let entry = format!("{:44} | {:?}", Hash::from(k.state_key()), v.value,);
+            let entry = format!("{:44} | {:?}", Hash::from(k.state_key()), v);
             for line in entry.lines() {
                 println!("{ind} {}", line.cyan());
             }
