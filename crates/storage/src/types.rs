607e404cb31aae53132c3ff5e5aa56a330895405
Miraculous Owonubi <omiraculous@gmail.com>
2024-10-24 15:10:07 +0300

    fix CI

--- crates/storage/src/types.rs
+++ crates/storage/src/types.rs
@@ -1,21 +1,22 @@
 //! High-level data structures for storage.
 
-use std::{borrow::Borrow, marker::PhantomData};
+use std::borrow::Borrow;
+use std::marker::PhantomData;
 
-use crate::{
-    address::{Path, PathError},
-    entities::{Data, Element},
-    interface::{Interface, StorageError},
-    AtomicUnit, Collection,
-};
 use borsh::{BorshDeserialize, BorshSerialize};
 use thiserror::Error;
 
-use crate as calimero_storage; // macro expects `calimero_storage` to be in deps
+// fixme! macro expects `calimero_storage` to be in deps
+use crate as calimero_storage;
+use crate::address::{Path, PathError};
+use crate::entities::{Data, Element};
+use crate::interface::{Interface, StorageError};
+use crate::{AtomicUnit, Collection};
 
 /// General error type for storage operations while interacting with complex collections.
 #[derive(Debug, Error)]
-pub enum Error {
+#[non_exhaustive]
+pub enum StoreError {
     /// Error while interacting with storage.
     #[error(transparent)]
     StorageError(#[from] StorageError),
@@ -29,7 +30,9 @@ pub enum Error {
 #[type_id(255)]
 #[root]
 pub struct Map<K, V> {
+    /// The entries in the map.
     entries: Entries<K, V>,
+    /// The storage element for the map.
     #[storage]
     storage: Element,
 }
@@ -38,6 +41,7 @@ pub struct Map<K, V> {
 #[derive(Collection, Clone, Debug, Eq, PartialEq, PartialOrd)]
 #[children(Entry<K, V>)]
 struct Entries<K, V> {
+    /// Helper to associate the generic types with the collection.
     _priv: PhantomData<(K, V)>,
 }
 
@@ -45,15 +49,25 @@ struct Entries<K, V> {
 #[derive(AtomicUnit, Clone, Debug, Eq, PartialEq, PartialOrd)]
 #[type_id(254)]
 pub struct Entry<K, V> {
+    /// The key for the entry.
     key: K,
+    /// The value for the entry.
     value: V,
+    /// The storage element for the entry.
     #[storage]
     storage: Element,
 }
 
 impl<K: BorshSerialize + BorshDeserialize, V: BorshSerialize + BorshDeserialize> Map<K, V> {
     /// Create a new map collection.
-    pub fn new(path: &Path) -> Result<Self, Error> {
+    ///
+    /// # Errors
+    ///
+    /// If an error occurs when interacting with the storage system, or a child
+    /// [`Element`](crate::entities::Element) cannot be found, an error will be
+    /// returned.
+    ///
+    pub fn new(path: &Path) -> Result<Self, StoreError> {
         let mut this = Self {
             entries: Entries::default(),
             storage: Element::new(path),
@@ -65,7 +79,14 @@ impl<K: BorshSerialize + BorshDeserialize, V: BorshSerialize + BorshDeserialize>
     }
 
     /// Insert a key-value pair into the map.
-    pub fn insert(&mut self, key: K, value: V) -> Result<(), Error> {
+    ///
+    /// # Errors
+    ///
+    /// If an error occurs when interacting with the storage system, or a child
+    /// [`Element`](crate::entities::Element) cannot be found, an error will be
+    /// returned.
+    ///
+    pub fn insert(&mut self, key: K, value: V) -> Result<(), StoreError> {
         let path = self.path();
         // fixme! Reusing the Map's path for now. We "could" concatenate, but it's
         // fixme! non-trivial and currently non-functional, so it's been left out
@@ -91,19 +112,40 @@ impl<K: BorshSerialize + BorshDeserialize, V: BorshSerialize + BorshDeserialize>
     }
 
     /// Get an iterator over the entries in the map.
-    pub fn entries(&self) -> Result<impl Iterator<Item = (K, V)>, Error> {
+    ///
+    /// # Errors
+    ///
+    /// If an error occurs when interacting with the storage system, or a child
+    /// [`Element`](crate::entities::Element) cannot be found, an error will be
+    /// returned.
+    ///
+    pub fn entries(&self) -> Result<impl Iterator<Item = (K, V)>, StoreError> {
         let entries = Interface::children_of(self.id(), &self.entries)?;
 
         Ok(entries.into_iter().map(|entry| (entry.key, entry.value)))
     }
 
     /// Get the number of entries in the map.
-    pub fn len(&self) -> Result<usize, Error> {
+    ///
+    /// # Errors
+    ///
+    /// If an error occurs when interacting with the storage system, or a child
+    /// [`Element`](crate::entities::Element) cannot be found, an error will be
+    /// returned.
+    ///
+    pub fn len(&self) -> Result<usize, StoreError> {
         Ok(Interface::child_info_for(self.id(), &self.entries)?.len())
     }
 
     /// Get the value for a key in the map.
-    pub fn get<Q>(&self, key: &Q) -> Result<Option<V>, Error>
+    ///
+    /// # Errors
+    ///
+    /// If an error occurs when interacting with the storage system, or a child
+    /// [`Element`](crate::entities::Element) cannot be found, an error will be
+    /// returned.
+    ///
+    pub fn get<Q>(&self, key: &Q) -> Result<Option<V>, StoreError>
     where
         K: Borrow<Q>,
         Q: PartialEq + ?Sized,
@@ -118,7 +160,14 @@ impl<K: BorshSerialize + BorshDeserialize, V: BorshSerialize + BorshDeserialize>
     }
 
     /// Remove a key from the map, returning the value at the key if it previously existed.
-    pub fn remove<Q>(&mut self, key: &Q) -> Result<Option<V>, Error>
+    ///
+    /// # Errors
+    ///
+    /// If an error occurs when interacting with the storage system, or a child
+    /// [`Element`](crate::entities::Element) cannot be found, an error will be
+    /// returned.
+    ///
+    pub fn remove<Q>(&mut self, key: &Q) -> Result<Option<V>, StoreError>
     where
         K: Borrow<Q>,
         Q: PartialEq + ?Sized,
@@ -135,7 +184,14 @@ impl<K: BorshSerialize + BorshDeserialize, V: BorshSerialize + BorshDeserialize>
     }
 
     /// Clear the map, removing all entries.
-    pub fn clear(&mut self) -> Result<(), Error> {
+    ///
+    /// # Errors
+    ///
+    /// If an error occurs when interacting with the storage system, or a child
+    /// [`Element`](crate::entities::Element) cannot be found, an error will be
+    /// returned.
+    ///
+    pub fn clear(&mut self) -> Result<(), StoreError> {
         let entries = Interface::children_of(self.id(), &self.entries)?;
 
         for entry in entries {
