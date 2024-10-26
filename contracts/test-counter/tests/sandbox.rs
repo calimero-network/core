226c4633b71576301077815fe376c4269bd81cf2
Xabi Losada <xabilosada@gmail.com>
2024-10-26 14:20:25 +0200

    test: add test contract

---
+++ contracts/test-counter/tests/sandbox.rs
@@ -0,0 +1,37 @@
+use eyre::Result;
+use near_workspaces::{network::Sandbox, Worker, Contract};
+
+const CONTRACT_WASM: &str = "./res/test_counter_near.wasm";
+
+async fn deploy_counter_contract(worker: &Worker<Sandbox>) -> Result<Contract> {
+    let wasm = std::fs::read(CONTRACT_WASM)?;
+    let contract = worker.dev_deploy(&wasm).await?;
+    Ok(contract)
+}
+
+#[tokio::test]
+async fn test_counter_contract() -> Result<()> {
+    let worker = near_workspaces::sandbox().await?;
+    let counter_contract = deploy_counter_contract(&worker).await?;
+
+    let _res = counter_contract
+        .call("new")
+        .transact()
+        .await?
+        .into_result()?; 
+
+    let _res = counter_contract
+        .call("increment")
+        .transact()
+        .await?
+        .into_result()?;
+
+    let counter_value: u32 = counter_contract
+        .view("get_count")
+        .await?
+        .json()?;
+
+    assert_eq!(counter_value, 1, "Counter should be incremented once");
+
+    Ok(())
+}
