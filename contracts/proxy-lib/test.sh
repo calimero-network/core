8c6d4b37f93b438c43ab0a3abef849150af12118
Xabi Losada <xabilosada@gmail.com>
2024-10-31 14:14:24 +0100

    fix: update test script

--- contracts/proxy-lib/test.sh
+++ contracts/proxy-lib/test.sh
@@ -1,18 +1,7 @@
 #!/bin/sh
 set -e
 
-PROXY_CONTRACT_PATH="./"
-TEST_CONTRACT_PATH="../test-counter"
-CONTEXT_CONFIG_CONTRACT_PATH="../context-config"
-
-echo "Building proxy contract..."
-(cd $PROXY_CONTRACT_PATH && ./build.sh)
-
-echo "Building proxy contract..."
-(cd $TEST_CONTRACT_PATH && ./build.sh)
-
-echo "Building context-config contract..."
-(cd $CONTEXT_CONFIG_CONTRACT_PATH && ./build.sh)
+./build-test-deps.sh
 
 echo "Running tests..."
 cargo test -- --nocapture
