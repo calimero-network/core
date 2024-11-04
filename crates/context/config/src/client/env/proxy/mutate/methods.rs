7049003b30c6f485e5946ca8190564b2ecd4b496
Xabi Losada <xabilosada@gmail.com>
2024-11-04 07:38:48 +0100

    feat: implement proxy approval

--- crates/context/config/src/client/env/proxy/mutate/methods.rs
+++ crates/context/config/src/client/env/proxy/mutate/methods.rs
@@ -1,7 +1,7 @@
 use super::{ContextProxyMutate, ContextProxyMutateRequest};
 use crate::repr::Repr;
 use crate::types::SignerId;
-use crate::{Proposal, ProposalAction, ProxyMutateRequest};
+use crate::{Proposal, ProposalAction, ProposalApprovalWithSigner, ProposalId, ProxyMutateRequest};
 
 impl<'a, T> ContextProxyMutate<'a, T> {
     pub fn propose(
@@ -15,6 +15,23 @@ impl<'a, T> ContextProxyMutate<'a, T> {
                 proposal: Proposal {
                     author_id: Repr::new(author_id),
                     actions,
+                },
+            },
+        }
+    }
+
+    pub fn approve(
+        self,
+        signer_id: Repr<SignerId>,
+        proposal_id: ProposalId,
+    ) -> ContextProxyMutateRequest<'a, T> {
+        ContextProxyMutateRequest {
+            client: self.client,
+            raw_request: ProxyMutateRequest::Approve {
+                approval: ProposalApprovalWithSigner {
+                    proposal_id,
+                    signer_id,
+                    added_timestamp: 0, // TODO: add timestamp
                 },
             },
         }
