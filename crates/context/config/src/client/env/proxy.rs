798bb9c1543fd306537f83cbf16e20015d5bc906
Miraculous Owonubi <omiraculous@gmail.com>
2024-11-05 10:52:00 +0300

    cleanup

--- crates/context/config/src/client/env/proxy.rs
+++ crates/context/config/src/client/env/proxy.rs
@@ -7,9 +7,9 @@ use mutate::ContextProxyMutate;
 use query::ContextProxyQuery;
 
 #[derive(Copy, Clone, Debug)]
-pub enum ContextConfig {}
+pub enum ContextProxy {}
 
-impl<'a, T: 'a> Environment<'a, T> for ContextConfig {
+impl<'a, T: 'a> Environment<'a, T> for ContextProxy {
     type Query = ContextProxyQuery<'a, T>;
     type Mutate = ContextProxyMutate<'a, T>;
 
