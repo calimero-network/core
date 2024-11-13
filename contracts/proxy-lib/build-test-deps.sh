677801d53d95bfbd65a93876d191b25fd229a268
Miraculous Owonubi <omiraculous@gmail.com>
2024-11-13 10:11:32 +0300

    fix tests

--- contracts/proxy-lib/build-test-deps.sh
+++ contracts/proxy-lib/build-test-deps.sh
@@ -8,10 +8,10 @@ TEST_CONTRACT_PATH="../test-counter"
 CONTEXT_CONFIG_CONTRACT_PATH="../context-config"
 
 echo "Building proxy contract..."
-$PROXY_CONTRACT_PATH/build.sh
+$PROXY_CONTRACT_PATH/build.sh --test
 
-echo "Building proxy contract..."
-$TEST_CONTRACT_PATH/build.sh --test
+echo "Building test-counter contract..."
+$TEST_CONTRACT_PATH/build.sh
 
 echo "Building context-config contract..."
 $CONTEXT_CONFIG_CONTRACT_PATH/build.sh
