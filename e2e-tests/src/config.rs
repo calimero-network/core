7fc10d444ead60de1d092419e86511de7f6969f8
Fico <70634661+fbozic@users.noreply.github.com>
2024-11-19 19:01:46 +0100

    feat(e2e-tests): use near sandbox worker (#967)

--- e2e-tests/src/config.rs
+++ e2e-tests/src/config.rs
@@ -1,3 +1,4 @@
+use camino::Utf8PathBuf;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
@@ -5,6 +6,7 @@ use serde::{Deserialize, Serialize};
 pub struct Config {
     pub network: Network,
     pub merod: MerodConfig,
+    pub near: Near,
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
@@ -19,4 +21,11 @@ pub struct Network {
 #[serde(rename_all = "camelCase")]
 pub struct MerodConfig {
     pub args: Box<[String]>,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Near {
+    pub context_config_contract: Utf8PathBuf,
+    pub proxy_lib_contract: Utf8PathBuf,
 }
