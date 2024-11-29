ad04e9d1c81a9b1fc6b1f2bc15796664de650027
alenmestrov <alenmestrov@gmail.com>
2024-11-29 19:19:42 +0100

    fix: changed the path to wasm file of contract for CI tests

--- contracts/icp/context-config/tests/integration.rs
+++ contracts/icp/context-config/tests/integration.rs
@@ -13,7 +13,7 @@ use serde_json;
 
 fn setup() -> (PocketIc, Principal) {
     let pic = PocketIc::new();
-    let wasm = std::fs::read(".dfx/local/canisters/context_contract/context_contract.wasm")
+    let wasm = std::fs::read("target/wasm32-unknown-unknown/release/context_contract.wasm")
         .expect("failed to read wasm");
     let canister = pic.create_canister();
     pic.add_cycles(canister, 2_000_000_000_000);
