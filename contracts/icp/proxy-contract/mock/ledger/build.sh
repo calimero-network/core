a851c94b823839bbd8dd6c97edce32782b66f414
alenmestrov <alenmestrov@gmail.com>
2024-12-04 16:20:32 +0100

    fix: removed Config.toml files and updated the scripts

--- contracts/icp/proxy-contract/mock/ledger/build.sh
+++ contracts/icp/proxy-contract/mock/ledger/build.sh
@@ -3,7 +3,7 @@ set -e
 
 cd "$(dirname $0)"
 
-TARGET="${CARGO_TARGET_DIR:-./target}"
+TARGET="${CARGO_TARGET_DIR:-../../../../../target}"
 
 rustup target add wasm32-unknown-unknown
 
@@ -11,7 +11,7 @@ cargo build --target wasm32-unknown-unknown --profile app-release
 
 mkdir -p res
 
-cp $TARGET/wasm32-unknown-unknown/app-release/mock_ledger.wasm ./res/mock_ledger.wasm
+cp $TARGET/wasm32-unknown-unknown/app-release/mock_ledger.wasm ./res/
 
 if command -v wasm-opt > /dev/null; then
   wasm-opt -Oz ./res/mock_ledger.wasm -o ./res/mock_ledger.wasm
