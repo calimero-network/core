bdd463ffbffafb5b53a3cb9211ce45a308796a33
Filip Bozic <70634661+fbozic@users.noreply.github.com>
2025-02-11 15:31:24 +0100

    ci: skip contract and apps build due to gh issues

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,14 +5,14 @@ set -ex
 
 cd "$(dirname $0)"
 
-# prepare apps
-./build-all-apps.sh
+# # prepare apps
+# ./build-all-apps.sh
 
-# Prepare contracts
-../contracts/near/registry/build.sh
-../contracts/near/context-config/build.sh
-../contracts/near/context-proxy/build-test-deps.sh
-../contracts/icp/context-config/build.sh
-../contracts/icp/context-proxy/build_contracts.sh
+# # Prepare contracts
+# ../contracts/near/registry/build.sh
+# ../contracts/near/context-config/build.sh
+# ../contracts/near/context-proxy/build-test-deps.sh
+# ../contracts/icp/context-config/build.sh
+# ../contracts/icp/context-proxy/build_contracts.sh
 # Run cargo test
 cargo test
