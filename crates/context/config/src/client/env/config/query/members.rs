a486685456dea8fed10150a028b8289093d1d490
alenmestrov <alenmestrov@gmail.com>
2025-02-11 09:10:27 +0100

    trying as_bytes()

--- crates/context/config/src/client/env/config/query/members.rs
+++ crates/context/config/src/client/env/config/query/members.rs
@@ -138,10 +138,7 @@ impl Method<Stellar> for MembersRequest {
 
     fn encode(self) -> eyre::Result<Vec<u8>> {
         let env = Env::default();
-        let context_id: [u8; 32] = self
-            .context_id
-            .rt()
-            .map_err(|e| eyre::eyre!("cannot convert context id to raw bytes: {}", e))?;
+        let context_id: [u8; 32] = self.context_id.as_bytes();
         let context_id_val: BytesN<32> = context_id.into_val(&env);
 
         let offset_val: u32 = self.offset as u32;
