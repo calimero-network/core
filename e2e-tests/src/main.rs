8588617cebdb34b5d04ef51c7326ba2efa8ba7b6
alenmestrov <alenmestrov@gmail.com>
2025-02-12 21:00:03 +0100

    fix: added protocols to env

--- e2e-tests/src/main.rs
+++ e2e-tests/src/main.rs
@@ -76,6 +76,7 @@ pub struct TestEnvironment {
     pub logs_dir: Utf8PathBuf,
     pub icp_dir: Utf8PathBuf,
     pub output_writer: OutputWriter,
+    pub protocols: Vec<String>,
 }
 
 impl From<Args> for TestEnvironment {
@@ -92,6 +93,7 @@ impl From<Args> for TestEnvironment {
             logs_dir: val.output_dir.join("logs"),
             icp_dir: val.output_dir.join("icp"),
             output_writer: OutputWriter::new(val.output_format),
+            protocols: val.protocols,
         }
     }
 }
