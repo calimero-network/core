89a99bcf7967b1ef0e51fbc84101cdb33435b619
Miraculous Owonubi <omiraculous@gmail.com>
2025-02-24 14:42:44 +0300

    feat: accomodate consuming streams owned by the actor state (#1130)

---
+++ crates/utils/actix/src/macros.rs
@@ -0,0 +1,142 @@
+#[cfg(test)]
+#[path = "macros_tests.rs"]
+mod macros_tests;
+
+#[doc(hidden)]
+pub mod __private {
+    #![allow(dead_code, unused_imports)]
+
+    pub use std::boxed::Box;
+    pub use std::ops::DerefMut;
+    pub use std::ptr;
+
+    pub use crate::spawn_actor;
+    pub use actix::{AsyncContext, Context, Handler, Message, StreamHandler};
+    pub use futures_util::{pin_mut, FutureExt, Stream, StreamExt};
+    pub use paste::paste;
+    pub use tokio::{select, task};
+
+    #[derive(Debug, Message)]
+    #[rtype("()")]
+    pub enum FromStreamInner<T> {
+        Started,
+        Finished,
+        Value(T),
+    }
+
+    impl FromStreamInner<()> {
+        pub const fn scoped_into<U, S>(this: FromStreamInner<U>, _: &S) -> FromStreamInner<U>
+        where
+            S: Stream<Item: Into<U>>,
+        {
+            this
+        }
+
+        pub const fn scoped_identity<S: Stream>(
+            this: FromStreamInner<S::Item>,
+            _: &S,
+        ) -> FromStreamInner<S::Item> {
+            this
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! spawn_actor {
+    (@$type:ident ? $($fn1:ident)::+ : $($fn2:ident)::+) => {
+        $($fn1)::+::<$type, _>
+    };
+    (@? $($fn1:ident)::+ : $($fn2:ident)::+) => {
+        $($fn2)::+
+    };
+    ($self:ident @ Self $($rest:tt),*) => {
+        compile_error!("`Self` is not allowed")
+    };
+    ($self:ident @ $actor:ident $(=> {$(.$stream:ident $(as $type:ident)?),+ $(,)?})?) => {{
+        use $crate::macros::__private::*;
+
+        paste! {
+            $($(
+                let [<stream_ $stream>] = {
+                    let stream = Box::deref_mut(&mut $self.$stream);
+                    unsafe { &mut *ptr::from_mut(stream) }
+                };
+            )+)?
+        }
+
+        let ctx = Context::new();
+
+        let addr = ctx.address();
+
+        let mut fut = ctx.into_future($self);
+
+        #[allow(non_local_definitions)]
+        impl<T> Handler<FromStreamInner<T>> for $actor
+        where
+            Self: StreamHandler<T>,
+        {
+            type Result = ();
+
+            fn handle(&mut self, msg: FromStreamInner<T>, ctx: &mut Context<Self>) -> Self::Result {
+                match msg {
+                    FromStreamInner::Started => StreamHandler::<T>::started(self, ctx),
+                    FromStreamInner::Finished => StreamHandler::<T>::finished(self, ctx),
+                    FromStreamInner::Value(value) => StreamHandler::<T>::handle(self, value, ctx),
+                }
+            }
+        }
+
+        let _ignored = task::spawn_local({
+            paste! {
+                $($(
+                    let [<task_ $stream>] = {
+                        let func = spawn_actor!(@ $($type)? ? FromStreamInner::scoped_into : FromStreamInner::scoped_identity);
+
+                        addr.do_send(func(FromStreamInner::Started, [<stream_ $stream>]));
+
+                        let addr = addr.downgrade();
+
+                        async move {
+                            loop {
+                                let item = [<stream_ $stream>].next().await;
+
+                                let Some(addr) = addr.upgrade() else {
+                                    break;
+                                };
+
+                                let Some(value) = item else {
+                                    addr.do_send(func(FromStreamInner::Finished, [<stream_ $stream>]));
+                                    break;
+                                };
+
+                                addr.do_send(func(FromStreamInner::Value(value.into()), [<stream_ $stream>]));
+                            }
+                        }
+                    };
+                )+)?
+            }
+
+            async move {
+                paste! {
+                    $($(
+                        pin_mut!([<task_ $stream>]);
+
+                        let mut [<task_ $stream>] = [<task_ $stream>].fuse();
+                    )+)?
+                }
+
+                loop {
+                    paste! {
+                        select! {
+                            biased;
+                            _ = &mut fut => { break },
+                            $($( _ = &mut [<task_ $stream>] => {} )+)?
+                        }
+                    }
+                }
+            }
+        });
+
+        addr
+    }};
+}
---
+++ crates/utils/actix/src/macros_tests.rs
@@ -0,0 +1,111 @@
+use std::array::IntoIter;
+
+use actix::{Actor, Addr, Context, Handler, Message, Response, StreamHandler};
+use futures_util::stream::{self, Iter, Repeat, StreamExt, Take, Zip};
+use tokio::time::{self, Instant};
+use tokio_stream::wrappers::IntervalStream;
+
+use crate::spawn_actor;
+
+struct MyActor {
+    total: usize,
+    stream1: Box<Take<Repeat<usize>>>,
+    current_name: &'static str,
+    stream2: Box<Zip<IntervalStream, Iter<IntoIter<&'static str, 26>>>>,
+}
+
+impl Actor for MyActor {
+    type Context = Context<Self>;
+
+    fn start(mut self) -> Addr<Self> {
+        spawn_actor!(self @ MyActor => {
+            .stream1,
+            .stream2 as Name,
+        })
+    }
+}
+
+impl StreamHandler<usize> for MyActor {
+    fn handle(&mut self, item: usize, _: &mut Context<Self>) {
+        self.total += item;
+    }
+
+    fn finished(&mut self, _ctx: &mut Self::Context) {}
+}
+
+struct Name {
+    name: &'static str,
+}
+
+impl From<(Instant, &'static str)> for Name {
+    fn from((_, name): (Instant, &'static str)) -> Self {
+        Self { name }
+    }
+}
+
+impl StreamHandler<Name> for MyActor {
+    fn handle(&mut self, item: Name, _: &mut Context<Self>) {
+        self.current_name = item.name;
+    }
+
+    fn finished(&mut self, _ctx: &mut Self::Context) {}
+}
+
+#[derive(Message)]
+#[rtype(usize)]
+struct GetTotal;
+
+impl Handler<GetTotal> for MyActor {
+    type Result = usize;
+
+    fn handle(&mut self, _: GetTotal, _: &mut Context<Self>) -> Self::Result {
+        self.total
+    }
+}
+
+#[derive(Message)]
+#[rtype("&'static str")]
+struct GetCurrentName;
+
+impl Handler<GetCurrentName> for MyActor {
+    type Result = Response<&'static str>;
+
+    fn handle(&mut self, _: GetCurrentName, _: &mut Context<Self>) -> Self::Result {
+        Response::reply(self.current_name)
+    }
+}
+
+#[actix::test]
+async fn test() -> eyre::Result<()> {
+    let names = stream::iter([
+        "Alice", "Bob", "Carol", "Dave", "Eve", "Frank", "Grace", "Heidi", "Ivan", "Judy", "Kevin",
+        "Larry", "Mallory", "Nancy", "Olivia", "Peggy", "Quentin", "Ralph", "Steve", "Trent",
+        "Ursula", "Victor", "Walter", "Xavier", "Yvonne", "Zelda",
+    ]);
+
+    let interval = time::interval(time::Duration::from_millis(200));
+
+    let addr = MyActor {
+        total: 0,
+        stream1: Box::new(stream::repeat(10).take(5)),
+        current_name: "",
+        stream2: Box::new(IntervalStream::new(interval).zip(names)),
+    }
+    .start();
+
+    let mut interval = time::interval(time::Duration::from_secs(2));
+
+    let _ignored = interval.tick().await;
+
+    let total = addr.send(GetTotal).await?;
+
+    assert_eq!(50, total);
+
+    let _ignored = interval.tick().await;
+
+    let e = addr.send(GetCurrentName).await?;
+
+    assert_eq!("Judy", e);
+
+    Ok(())
+}
