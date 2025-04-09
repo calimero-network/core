c4256eb5b631b439058060d3005c0ef7d5b61572
cy4n1d3-p1x3l <mradul.singhal1101@gmail.com>
2025-04-09 17:24:34 +0530

    aliases added to meroctl

--- crates/meroctl/src/cli/context/join.rs
+++ crates/meroctl/src/cli/context/join.rs
@@ -1,12 +1,15 @@
-use calimero_primitives::context::ContextInvitationPayload;
-use calimero_primitives::identity::PrivateKey;
+use calimero_primitives::alias::Alias;
+use calimero_primitives::context::{ContextId, ContextInvitationPayload};
+use calimero_primitives::identity::{PrivateKey, PublicKey};
 use calimero_server_primitives::admin::{JoinContextRequest, JoinContextResponse};
 use clap::Parser;
 use eyre::Result as EyreResult;
 use reqwest::Client;
 
 use crate::cli::Environment;
-use crate::common::{do_request, fetch_multiaddr, load_config, multiaddr_to_url, RequestType};
+use crate::common::{
+    create_alias, do_request, fetch_multiaddr, load_config, multiaddr_to_url, RequestType,
+};
 use crate::output::Report;
 
 #[derive(Debug, Parser)]
@@ -21,7 +24,12 @@ pub struct JoinCommand {
         value_name = "INVITE",
         help = "The invitation payload for joining the context"
     )]
+    // Is it any useful here?
+    #[clap(long = "name", help = "The alias for the context")]
+    pub name: Option<Alias<ContextId>>,
     pub invitation_payload: ContextInvitationPayload,
+    #[clap(long = "as", help = "The alias for the invitee")]
+    pub identity: Option<Alias<PublicKey>>,
 }
 
 impl Report for JoinContextResponse {
@@ -39,10 +47,11 @@ impl Report for JoinContextResponse {
 impl JoinCommand {
     pub async fn run(self, environment: &Environment) -> EyreResult<()> {
         let config = load_config(&environment.args.home, &environment.args.node_name)?;
+        let multiaddr = fetch_multiaddr(&config)?;
 
         let response: JoinContextResponse = do_request(
             &Client::new(),
-            multiaddr_to_url(fetch_multiaddr(&config)?, "admin-api/dev/contexts/join")?,
+            multiaddr_to_url(multiaddr, "admin-api/dev/contexts/join")?,
             Some(JoinContextRequest::new(
                 self.private_key,
                 self.invitation_payload,
@@ -53,6 +62,23 @@ impl JoinCommand {
         .await?;
 
         environment.output.write(&response);
+
+        if let Some(ref payload) = response.data {
+            if let Some(identity) = self.identity {
+                let context_id = payload.context_id;
+                let public_key = payload.member_public_key;
+
+                let res = create_alias(
+                    multiaddr,
+                    &config.identity,
+                    identity,
+                    Some(context_id),
+                    public_key,
+                )
+                .await?;
+                environment.output.write(&res);
+            }
+        }
 
         Ok(())
     }
