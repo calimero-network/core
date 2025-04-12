1d40c3a156295e30712af8bb6ef0e59a4449fa1a
Miraculous Owonubi <omiraculous@gmail.com>
2025-04-12 21:50:03 +0100

    feat: introduce `app::log!` convenience macro (#1198)

--- crates/sdk/src/macros.rs
+++ crates/sdk/src/macros.rs
@@ -28,3 +28,19 @@ macro_rules! __bail__ {
         return ::core::result::Result::Err($crate::__err__!($fmt, $($arg)*));
     };
 }
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! __log__ {
+    ($fmt:literal $(,)?) => {
+        match ::std::format_args!($fmt) {
+            args => match args.as_str() {
+                Some(msg) => $crate::env::log(msg),
+                None => $crate::env::log(&::std::fmt::format(args)),
+            },
+        }
+    };
+    ($fmt:expr, $($arg:tt)*) => {
+        $crate::env::log(&::std::format!($fmt, $($arg)*))
+    };
+}
