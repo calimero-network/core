37c718f9f6b19fbbf0a33ae17c1e22d6546c562c
Nathy-bajo <bajon7680@gmail.com>
2025-04-18 19:43:42 +0100

    nit

--- e2e-tests/src/driver.rs
+++ e2e-tests/src/driver.rs
@@ -303,13 +303,13 @@ impl Driver {
 
                 merod.run().await?;
 
-                let node_name = e.insert(merod);
+                let merod = e.insert(merod);
 
                 while let Err(_) = try_join!(
                     TcpSocket::new_v4()?.connect(swarm_addr),
                     TcpSocket::new_v4()?.connect(server_addr)
                 ) {
-                    if let Some(exit_code) = node_name.try_wait().await? {
+                    if let Some(exit_code) = merod.try_wait().await? {
                         bail!(
                             "merod process exited with code {} before becoming ready",
                             exit_code
