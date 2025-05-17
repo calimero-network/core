37086ca521a29e5f179126a8023db6ca2b822b35
JesusOfLagos <jesuswrites20043@gmail.com>
2025-05-17 02:09:46 +0100

    test build with CI

--- crates/version/src/lib.rs
+++ crates/version/src/lib.rs
@@ -1,9 +1,9 @@
 pub fn version_info() -> String {
     format!(
         "calimero {} (build {}, commit {}, rustc {})",
-        env!("CARGO_PKG_VERSION"),
-        env!("GIT_DESCRIBE"),
-        env!("GIT_COMMIT_HASH"),
-        env!("RUSTC_VERSION"),
+        std::env::var("CARGO_PKG_VERSION"),
+        std::env::var("GIT_DESCRIBE"),
+        std::env::var("GIT_COMMIT_HASH"),
+        std::env::var("RUSTC_VERSION"),
     )
 }
