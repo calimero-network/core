219360fb3188f6a80d519316761b7acb13858fc5
iTranscend <llordbanks500@gmail.com>
2025-07-03 14:15:15 +0100

    feat: app building with cargo mero build

--- crates/cargo-mero/src/build.rs
+++ crates/cargo-mero/src/build.rs
@@ -1,3 +1,91 @@
+use std::fs;
+use std::path::PathBuf;
+use std::process::Stdio;
+
+use cargo_metadata::MetadataCommand;
+use eyre::{bail, Context};
+
+use crate::utils::new_command;
+
 pub async fn run() -> eyre::Result<()> {
+    let output = new_command("rustup")
+        .arg("target")
+        .arg("add")
+        .arg("wasm32-unknown-unknown")
+        .output()
+        .await
+        .wrap_err("Adding wasm32-unknown-unknown target failed")?;
+
+    if !output.status.success() {
+        bail!("Adding wasm32-unknown-unknown target command failed");
+    }
+
+    let output = new_command("cargo")
+        .arg("build")
+        .arg("--target")
+        .arg("wasm32-unknown-unknown ")
+        .arg("--profile")
+        .arg("app-release")
+        .output()
+        .await
+        .wrap_err("cargo build failed")?;
+
+    if !output.status.success() {
+        bail!("cargo build command failed");
+    }
+
+    // Copy wasm to res folder
+    fs::create_dir("res")?;
+
+    let package_name = MetadataCommand::new().exec()?.packages[0]
+        .name
+        .clone()
+        .into_inner()
+        .replace("-", "_");
+    let wasm_file = format!("{}.wasm", package_name);
+    let wasm_path = build_path("./target/wasm32-unknown-unknown/app-release/", &wasm_file);
+
+    let _ = fs::copy(wasm_path, build_path("./res/", &wasm_file))?;
+
+    // Optimize wasm if wasm-opt is present
+    if wasm_opt_installed().await {
+        println!("wasm-opt found, optimizing...");
+
+        let output = new_command("wasm-opt")
+            .arg("-Oz")
+            .arg(build_path("./res/", &wasm_file))
+            .arg("-o")
+            .arg(build_path("./res/", &wasm_file))
+            .output()
+            .await?;
+
+        if !output.status.success() {
+            bail!(
+                "wasm optimization command failed -> code: {:?},\n stderr: {:#?} \n stdout: {:?} \n",
+                output.status.code(),
+                String::from_utf8(output.stderr),
+                output.stdout
+            );
+        }
+        println!("wasm optimization complete");
+    } else {
+        println!("wasm-opt not found, skipping optimization");
+    }
     Ok(())
+}
+
+async fn wasm_opt_installed() -> bool {
+    new_command("wasm-opt")
+        .arg("--version")
+        .stdout(Stdio::null())
+        .status()
+        .await
+        .is_ok()
+}
+
+/// Builds a path from two `&str`s
+fn build_path(a: &str, b: &str) -> PathBuf {
+    let mut path = PathBuf::from(a);
+    path.push(b);
+    path
 }
--- crates/cargo-mero/src/main.rs
+++ crates/cargo-mero/src/main.rs
@@ -4,6 +4,7 @@ mod abi;
 mod build;
 mod cli;
 mod new;
+mod utils;
 
 async fn init() -> eyre::Result<()> {
     let command = cli::RootCommand::parse();
