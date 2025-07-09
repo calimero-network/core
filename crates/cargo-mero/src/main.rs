8ac7f658429011b8d938408e9d9e50a230186236
iTranscend <llordbanks500@gmail.com>
2025-07-09 10:08:15 +0100

    refactor:  merge init into main

--- crates/cargo-mero/src/main.rs
+++ crates/cargo-mero/src/main.rs
@@ -4,15 +4,8 @@ mod build;
 mod cli;
 mod new;
 
-async fn init() -> eyre::Result<()> {
-    let command = cli::RootCommand::parse();
-
-    command.run().await
-}
-
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
-    init().await?;
-
-    Ok(())
+    let command = cli::RootCommand::parse();
+    command.run().await
 }
