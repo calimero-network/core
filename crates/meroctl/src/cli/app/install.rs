1b78d097733af759bf214933b3f01cb6ab32c2af
Seth <sethryanrollins@gmail.com>
2025-07-18 15:50:38 -0400

    Revert "feat: add stream upload support for applications"
    This reverts commit bcc90b83a19a1eb12e3b51f717cb6ba1e48bee4d.

--- crates/meroctl/src/cli/app/install.rs
+++ crates/meroctl/src/cli/app/install.rs
@@ -65,7 +65,7 @@ impl InstallCommand {
 
     /// Install an application by reading data from standard input.
     ///
-    ///  Eg:
+    /// Example usage:
     /// `cat app.wasm | meroctl app install --stdin`
     ///
     /// The implementation reads all available data from stdin, encodes its metadata as base64 and sends the binary data
