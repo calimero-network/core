2b43f20be933c53ccb96708e431d5fb48f2dd523
iTranscend <llordbanks500@gmail.com>
2025-07-31 15:01:14 +0100

    fix: pass aggregates by reference for wasm_c_abi transition

---
+++ crates/sdk/src/sys/types/ref.rs
@@ -0,0 +1,26 @@
+use std::marker::PhantomData;
+use std::ptr;
+
+use crate::sys::PtrSizedInt;
+
+/// Abstraction type that encodes the pointer to any aggregate type T
+#[repr(C)]
+pub struct Ref<T> {
+    ptr: PtrSizedInt,
+    phantom: PhantomData<T>,
+}
+
+impl<T> Ref<T> {
+    pub fn new(val: &T) -> Self {
+        Ref {
+            ptr: PtrSizedInt::new(ptr::from_ref(val).addr()),
+            phantom: PhantomData,
+        }
+    }
+}
+
+impl<T> From<T> for Ref<T> {
+    fn from(value: T) -> Self {
+        Ref::new(&value)
+    }
+}
