24224e26cdb68740d7c0e481fdf7e106cbf4ab98
Septen <septengineering@pm.me>
2025-09-03 12:25:42 +0000

    Ghost commit to trigger the CI.

--- e2e-tests/src/main.rs
+++ e2e-tests/src/main.rs
@@ -30,6 +30,8 @@ pub const EXAMPLES: &str = r"
     --meroctl-binary ./target/debug/meroctl
 ";
 
+// Ghost comment to trigger the CI
+
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
 #[command(after_help = concatcp!(
