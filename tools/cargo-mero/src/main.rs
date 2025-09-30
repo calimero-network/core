921511c951e054ab9399f85faeb18f1b41c0154d
chefsale <sandi.fatic@gmail.com>
2025-10-01 00:13:09 +0200

    fix: pin tools version seperately

--- tools/cargo-mero/src/main.rs
+++ tools/cargo-mero/src/main.rs
@@ -3,11 +3,9 @@ use clap::Parser;
 mod build;
 mod cli;
 mod new;
-mod version;
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
-    version::check_for_update();
     let command = cli::RootCommand::parse();
     command.run().await
 }
