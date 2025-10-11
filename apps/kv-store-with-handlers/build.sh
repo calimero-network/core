15fd378e088de179e06eb74cf1d12f695a1e943d
Sandi Fatic <sandi@tenzen.io>
2025-10-11 17:55:55 +0200

    feat: added simple callbacks with e2e tests (#1536)

---
+++ apps/kv-store-with-handlers/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname $0)"
+
+TARGET="${CARGO_TARGET_DIR:-../../target}"
+
+rustup target add wasm32-unknown-unknown
+
+mkdir -p res
+
+cargo build --target wasm32-unknown-unknown --profile app-release
+
+cp $TARGET/wasm32-unknown-unknown/app-release/kv_store_with_handlers.wasm ./res/
+
+if command -v wasm-opt > /dev/null; then
+  wasm-opt -Oz ./res/kv_store_with_handlers.wasm -o ./res/kv_store_with_handlers.wasm
+fi
