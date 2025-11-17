4b045096e6f22c54df7d4351839a4215a020cf40
Filip Bozic <70634661+fbozic@users.noreply.github.com>
2025-11-17 12:58:47 +0100

    feat: add tee verify-quote endpoint, make all tee endpoints public

--- crates/server/src/admin/handlers/tee.rs
+++ crates/server/src/admin/handlers/tee.rs
@@ -1,11 +1,13 @@
 use axum::routing::{get, post};
 use axum::Router;
 
-mod attestation;
+mod attest;
 mod info;
+mod verify_quote;
 
 pub fn service() -> Router {
     Router::new()
         .route("/info", get(info::handler))
-        .route("/attest", post(attestation::handler))
+        .route("/attest", post(attest::handler))
+        .route("/verify-quote", post(verify_quote::handler))
 }
