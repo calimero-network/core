8ebbec0380ba1b9c3097987d417ff44e029f1b5c
Septen <septengineering@pm.me>
2025-12-28 23:50:08 +0000

    Update kv-store build script & app and strip more data.

--- apps/kv-store/build.sh
+++ apps/kv-store/build.sh
@@ -11,20 +11,30 @@ WORKSPACE_ROOT=$(cargo metadata --format-version 1 | jq -r '.workspace_root')
 CARGO_HOME_DIR="${CARGO_HOME:-$HOME/.cargo}"
 # Get the path to the Rust standard library source
 RUST_SYSROOT=$(rustc --print sysroot)
+# Get the full rustc commit hash from `rustc --version --verbose`
+RUST_COMMIT_HASH=$(rustc -Vv | grep commit-hash | cut -d ' ' -f2)
+RUST_MAP="rust"
+
+# Deep source path identification
+# This targets the actual 'library' folder to skip the `/lib/rustlib/src/rust/` nesting
+LOCAL_RUST_SRC="$RUST_SYSROOT/lib/rustlib/src/rust"
+VIRTUAL_RUST_SRC="/rustc/$RUST_COMMIT_HASH"
 
 rustup target add wasm32-unknown-unknown
 
 mkdir -p res
 
-RUSTFLAGS=" \
-  --remap-path-prefix $PROJECT_ROOT=project \
+export RUSTFLAGS=" \
+  --remap-path-prefix $WORKSPACE_ROOT=project \
   --remap-path-prefix $CARGO_HOME_DIR=cargo \
-  --remap-path-prefix $RUST_SYSROOT=/rustc \
-  -C debuginfo=0" \
+  --remap-path-prefix=$LOCAL_RUST_SRC=$RUST_MAP \
+  --remap-path-prefix=$VIRTUAL_RUST_SRC=$RUST_MAP \
+  -C debuginfo=0"
+
 cargo build --target wasm32-unknown-unknown --profile app-release
 
 cp $TARGET/wasm32-unknown-unknown/app-release/kv_store.wasm ./res/
 
 if command -v wasm-opt > /dev/null; then
-  wasm-opt -Oz --strip-debug ./res/kv_store.wasm -o ./res/kv_store.wasm
+  wasm-opt -Oz --strip-debug --strip-producers ./res/kv_store.wasm -o ./res/kv_store.wasm
 fi
