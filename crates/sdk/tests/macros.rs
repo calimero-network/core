ec6315ec7fb358ddf09211d3596c219b001e5220
Sandi Fatic <sandi@tenzen.io>
2026-06-02 18:14:47 +0200

    feat(sdk/macros): compile-error when a multi-writer/sync-semantic collection is used inside #[app::private] (#2628)

--- crates/sdk/tests/macros.rs
+++ crates/sdk/tests/macros.rs
@@ -51,4 +51,5 @@ fn all() {
     t.compile_fail("tests/macros/error_const_generic.rs");
     t.compile_fail("tests/macros/error_explicit_abi.rs");
     t.compile_fail("tests/macros/error_event_on_struct.rs");
+    t.compile_fail("tests/macros/error_private_incompatible.rs");
 }
